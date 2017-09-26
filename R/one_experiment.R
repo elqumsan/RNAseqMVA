2ff54f09fb0bdb09cadaff939f5a5acf6728e6ce
elqumsan <m.qumsan@gmail.com>
2017-09-26 17:34:34 +0200

    documentation of the one experiment for roxygen2

--- R/one_experiment.R
+++ R/one_experiment.R
@@ -1,6 +1,24 @@
-
+####################################################################################
+#' @title repeating the classifier with particular data type
+#' @author Mustafa ABUELQUMSAN and Jacques van Helden
+#' @description for sake of the accuracy and due to the error rate have computed from sampleing from the origin count data,
+#' # it is better to compute the the error rate multiple time and then we would find the avarage for the error rate.
+#' @param countTable is the passed count data for the iterative process.
+#' @param classes is the classes lable for the cout data.
+#' @param classifier is the type of classifier that is used with repeated process.
+#' @param data.type is data type that is used with iterative process, supported "raw","log2" and "log2norm"
+#' @param iteration is the how much number it will be repeated the process.
+#' @param variable.type this indicate for the number of variables that will used with iterative procedure.
+#' @param trainingProportion is the ratio of trianing proportion.
+#' @param permute is show if the class lable are permuted this for sake of the knowing the strength and weaknesses of the classifier
+#' @param file.prefix in order to let us to save file from the one experiment
+#' @param verbose to write messages to indicate the progressing of tasks
+#' @param k this parameter for the knn classifier to identify the number of neighbour in classification process.
+#'
+#' @return
+#' @export
 ################################################################
-## Define a function to iterate over one classifier with one particular data type. 
+## Define a function to iterate over one classifier with one particular data type.
 one.experiment <- function (countTable, # count table (may be normalized or not, log2 or not)
                             classes, ## Class labels attached to each sample
                             classifier, # supported: knn or rf
@@ -13,15 +31,15 @@ one.experiment <- function (countTable, # count table (may be normalized or not,
                             verbose=FALSE,
                             k=3 ## For KNN only
 ) {
-  
-  
+
+
   startTime <- Sys.time();
-  
+
   message(format(Sys.time(), "%Y-%m-%d_%H%M%S"), "\t", classifier, " classifier (train vs test), ", data.type, " counts, ", variable.type, " variables, ", parameters$iterations, " iterations.")
   testTable <- data.frame()
   # i <- 1
-  
-  
+
+
   ## Define file prefix is not specified in paramters
   if (is.null(file.prefix)) {
     file.prefix <- paste(sep="_", classifier, data.type, variable.type)
@@ -34,39 +52,39 @@ one.experiment <- function (countTable, # count table (may be normalized or not,
   outDirectory <- table.dirs[classifier]
   dir.create(outDirectory, recursive = TRUE, showWarnings = F)
   message("Output directory: ", outDirectory)
-  
-    
+
+
   ## Iterate traint/test cycles
   for (i in 1:iterations) {
     ## Permute class labels if required
     if (permute) {
       classes <- sample(classes)
     }
-    
+
     # computing the testing errors rate for the KNN classfier
     message("\t", format(Sys.time(), "%Y-%m-%d_%H%M%S"), "\t", classifier, " training/testing evaluation, iteration ", i , "/", iterations)
     oneTest <- MisclassificationEstimate(countTable, classes , trainingProportion , classifier = classifier, k=k, verbose=verbose)
     testTable <- rbind (testTable, oneTest$stats)
   }
-  
+
   ## Save the result table for KNN training/testing evaluation
   testTable.file <- file.path(outDirectory, paste(sep="", file.prefix, ".tsv"))
-  
-  write.table(file=testTable.file, 
+
+  write.table(file=testTable.file,
               x = testTable,
               row.names = TRUE, col.names = NA, sep="\t", quote=FALSE)
 
   ## Export a summary about the error rate
   errorSummary.file <- file.path(outDirectory, paste(sep="", file.prefix, "_summary.tsv"))
-  write.table(tidy(summary(testTable$testing.error.rate)), 
+  write.table(tidy(summary(testTable$testing.error.rate)),
               quote=FALSE, sep="\t", row.names=TRUE, col.names = NA,
-              file = errorSummary.file) 
-  
+              file = errorSummary.file)
+
   ## Plot a box plot of training versus testing error
   boxplot.file <- file.path(
-    outDirectory, 
+    outDirectory,
     paste(sep="", file.prefix, "_R", iterations, "_learning_vs_test_error_boxplot.pdf"))
-  
+
   pdf(file=boxplot.file, width = 7, height = 5)
   main <- paste(sep="", classifier, "; ", data.type, " counts; ", variable.type, " variables")
   if (permute) {
@@ -83,5 +101,5 @@ one.experiment <- function (countTable, # count table (may be normalized or not,
   elapsedTimeFile <- file.path(outDirectory, paste(sep="", file.prefix, "_elapsed_time.txt"))
   write(file = elapsedTimeFile, x = paste(startTime, endTime, elapsedTime))
   message("Elapsed Time file: ", elapsedTimeFile)
-  return(testTable)  
+  return(testTable)
 }
