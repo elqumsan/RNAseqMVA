35789ad7d545d31035194c784c09467b06f6a7cf
elqumsan <m.qumsan@gmail.com>
2017-09-26 16:38:09 +0200

    documentation of the normalize count table.

--- R/normalize_count_table.R
+++ R/normalize_count_table.R
@@ -1,31 +1,31 @@
 #' @title Normalize RNA-seq count table
-#' @author Jacques van Helden
+#' @author Jacques van Helden and Mustafa AbuElqumsan
 #' @description Normalize a table of raw counts from RNA-seq
-#' @param rawCounts a raw count table, with one column per sample and one row per gene. 
+#' @param rawCounts a raw count table, with one column per sample and one row per gene.
 #' Normalization will be done by samples (columns).
-#' @param method="quantile" normalization method. Supported: sum, mean, media, quantile. 
-#' Preferred method: quantile = 0.75. Quantiles are more robust to outliers than the mean 
-#' or sum. The median is sometimes weak in RNA-seq data, the 75th percentile (quantile 0.75) 
-#' seems a good tradeoff between robustness and representativity (taking into account a 
-#' representative proportion of the total counts per samples). 
+#' @param method="quantile" normalization method. Supported: sum, mean, media, quantile.
+#' Preferred method: quantile = 0.75. Quantiles are more robust to outliers than the mean
+#' or sum. The median is sometimes weak in RNA-seq data, the 75th percentile (quantile 0.75)
+#' seems a good tradeoff between robustness and representativity (taking into account a
+#' representative proportion of the total counts per samples).
 #' Note that median is the same as quantile 0.5.
-#' 
+#'
 #' @param quantile=0.75 quantile used as scaling factor when quantile method is selected.
 #' @param log2=TRUE apply a log2 transformation
 #' @param epsilon=0.1 value added to all counts before applying the log2 transformation
 #' in order to avoid zero counts.
-#' 
+#'
 #' @return a list with the normalized count table + a table providing sample-wise statistics
-#' 
-#' @examples 
-#' 
+#'
+#' @examples
+#'
 #' @export
 NormalizeCounts <- function(rawCounts,
                             method="quantile",
                             quantile=0.75,
                             log2=TRUE,
                             epsilon=0.1) {
-  
+
   ## Compute sample-wise statistics
   sampleStats <- data.frame(
     sum=apply(rawCounts, 2, sum, na.rm=TRUE),
@@ -36,7 +36,7 @@ NormalizeCounts <- function(rawCounts,
     Q3=apply(rawCounts, 2, quantile, na.rm=TRUE, probs=0.75),
     max=apply(rawCounts, 2, max, na.rm=TRUE)
   )
-  
+
   if (method == "quantile") {
     sampleStats$norm.quantile <- apply(rawCounts, 2, quantile, na.rm=TRUE, probs=quantile)
     sampleStats$scaling.factor <- sampleStats$norm.quantile
@@ -50,19 +50,19 @@ NormalizeCounts <- function(rawCounts,
   } else {
     stop(method, " is not a valid method for NormalizeCounts()")
   }
-  
+
   ## Compute normalised counts
   normTarget <- median(sampleStats$scaling.factor)
   normCounts <- t(t(rawCounts) / sampleStats$scaling.factor) * normTarget
-  
-  
+
+
   ## Run log2 transformation if required
   if (log2) {
     normCounts <- log2(normCounts + epsilon)
   }
-  
+
   # hist(unlist(normCounts), breaks=100)
-  
+
   result <- list()
   result$normCounts <- normCounts
   result$method <- method
