05de66bc996b5265e6559330b200c59ba01e08fe
elqumsan <m.qumsan@gmail.com>
2017-09-28 22:22:14 +0200

    change the classifier

--- misc/PCA_all_variables_vs_all_PCs.R
+++ misc/PCA_all_variables_vs_all_PCs.R
@@ -3,10 +3,10 @@
 ## All variables versus all PCs.
 ##
 ## QUESTION: is it better to use the PCAs-transformed data, and, if so, is it better to use a subset of the first components or all the components ?
-## For the time being we test this with only one classifier (KNN, default k)  but we will come back to it with other classifiers later. 
+## For the time being we test this with only one classifier (KNN, default k)  but we will come back to it with other classifiers later.
 
 ## Choice of the classifier
-classifier <- "rf" 
+classifier <- "svm"
 
 
 ## Choice of the coutns
@@ -18,15 +18,15 @@ data.type <- "log2norm.prcomp.centred"
 ## Default for quick test without iterating over all cases
 permute <- FALSE
 
-if (parameters$compute) { 
+if (parameters$compute) {
   ## List to store all results
   train.test.results.all.variables <- list()
   train.test.results.all.PCs <- list()
-  
-  message.with.time("Train/test all computations with constant training proportion :", 
+
+  message.with.time("Train/test all computations with constant training proportion :",
                     signif(parameters$trainingProportion, digits = 3) )
   message.with.time("\tTrain/test, k=", parameters$knn$k, "; classifier=", classifier)
-  
+
   ## Select the counts depending on the data type
   if (data.type == "raw") {
     counts <- rawCounts
@@ -35,27 +35,27 @@ if (parameters$compute) {
   } else if (data.type == "log2") {
     counts <- log2normCounts
   } else if (grepl("prc", data.type)) {
-    ## If we are not in the cases above, we assume that the data type is a 
-    ## PCA results, and we need to get the components. 
-    ## 
-    ## Create a variable name "counts" with the content of the variable whose 
+    ## If we are not in the cases above, we assume that the data type is a
+    ## PCA results, and we need to get the components.
+    ##
+    ## Create a variable name "counts" with the content of the variable whose
     ## name is given in "data.type"
     counts <- get(data.type)[["x"]]
   } else {
     stop(data.type, " is not a valid type, Supported: raw, norm, log2, and *pcr*")
   } # end else of other data.type
-  
-  
+
+
   ## Associate each analysis of real data with a permutation test
   for (permute in c(FALSE, TRUE)) {
-    
+
     ## Run classifier withb all variables (log2-transformed log counts)
     exp.prefix <-
       paste(sep = "_", classifier, "log2norm", "allvars")
     if (permute) {
       exp.prefix <- paste(sep = "_", exp.prefix, perm.prefix)
     }# end if permuted class
-    
+
     train.test.results.all.variables[[exp.prefix]] <-
       one.experiment (
         countTable = log2normCounts,
@@ -69,17 +69,17 @@ if (parameters$compute) {
         k = parameters$knn$k,
         verbose = parameters$verbose
       )
-    
+
     ## take all the principal components, and cast them to a data.frame
     first.pcs <- data.frame(counts)
-    
+
     ## define experiment prefix
     exp.prefix <-
       paste(sep = "_", classifier, data.type)
     if (permute) {
       exp.prefix <- paste(sep = "_", exp.prefix, perm.prefix)
     }# end if permuted class
-    
+
     train.test.results.all.variables[[exp.prefix]] <-
       one.experiment (
         countTable = first.pcs,
@@ -93,16 +93,16 @@ if (parameters$compute) {
         k = parameters$knn$k,
         verbose = parameters$verbose
       )
-    
-    #  } # end of iterative of PC Numbers 
-  } # end of permutation 
-} # end of computation   
+
+    #  } # end of iterative of PC Numbers
+  } # end of permutation
+} # end of computation
 
 ###############################################################################################
-## What is better to using all PCs versus all variables with KNN classifier? 
+## What is better to using all PCs versus all variables with KNN classifier?
 ErrorRateBoxPlot(experimentList = train.test.results.all.variables,
                  classifier = classifier,
-                 main = paste(classifier, ": all variables vs all PCs", "\n", 
-                              parameters$iterations, "iterations,", 
+                 main = paste(classifier, ": all variables vs all PCs", "\n",
+                              parameters$iterations, "iterations,",
                               data.type = data.type))
 
