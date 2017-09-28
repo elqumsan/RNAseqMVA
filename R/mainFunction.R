0923ca867cce99f8b2d638cfa4af7aab6060557e
elqumsan <m.qumsan@gmail.com>
2017-09-28 13:03:35 +0200

    the main function for all our used function in RNAseqMVA package

---
+++ R/mainFunction.R
@@ -0,0 +1,32 @@
+####################################################################
+#' @title main script to upload all required function that are necessary in RNAseqMVA package
+#' @author Mustafa ABUELQUMSAN and Jacques van-Helden
+#' @description we would like to ensure all function are uploaded in proper way to be aware all analysis steps will
+#' # work explicitly way.
+#' @export
+#'
+#'
+
+if (!exists(DEGordering ,  mode = "function")){
+ source("~/RNAseqMVA/R/deg_ordering.R")
+} else if(!exists(ErrorRateBoxPlot , mode = "function")){
+  source("~/RNAseqMVA/R/ErrorRateBoxPlot.R")
+} else if (!exists(loadCounts , mode = "function")){
+  source("~/RNAseqMVA/R/load_counts.R")
+}else if(!exists(loadRecountExperiment, mode = "function")){
+    source("~/RNAseqMVA/R/load_recount_experiment.R")
+}else if (!exists(MergeRuns , mode = "function")){
+    source("~/RNAseqMVA/R/merge_runs.R")
+}else if (!exists(MisclassificationEstimate, mode = "function")){
+    source("~/RNAseqMVA/R/misclassification_estimate.R")
+}else if( !exists(NormalizeCounts ,mode = "function")){
+    source("~/RNAseqMVA/R/normalize_count_table.R")
+}else if( !exists(one.experiment , mode = "function")){
+    source("~/RNAseqMVA/R/one_experiment.R")
+}else if(!exists(RequiredCRANPackages, mode = "function")){
+    source("~/RNAseqMVA/R/required_libraries.R")
+} else if (!exists(RequiredBioconductorPackages , mode = "function")){
+    source("~/RNAseqMVA/R/required_libraries.R")
+}else {
+    message("such function are not provided in the RNAseqMVA package,!!!")
+  }
