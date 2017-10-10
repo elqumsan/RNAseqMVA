34ce4f5de9c05f89fcecb9eb33bb73ae90d1b416
Mustafa AbuElQumsan <elqumsan@blackflag.local>
2017-10-10 13:31:42 +0200

    discard all

--- misc/DEG_impact_of_top_gene_number.R
+++ misc/DEG_impact_of_top_gene_number.R
@@ -5,7 +5,7 @@
 ###################################################################################################
 
 ## Choice of the classifier
-classifier <- "rf" 
+classifier <- "svm"
 
 
 ## Choice of the coutns
@@ -19,35 +19,35 @@ permute <- FALSE
 
 
 if (parameters$compute) {
-  message.with.time("Starting classification")  
-  
+  message.with.time("Starting classification")
+
   train.test.results.DEG <- list()
-  
+
  ## Associate all computation with permuted and not permuted class lables
   for (permute in c(FALSE, TRUE)) {
-    
+
     for (deg.method in parameters$deg.methods) {
       DEG <- get(paste(sep="", "DEG.", deg.method))
-      
+
       v  <- 5
       for(v in 1:length(parameters$nb.variables)){
         varnb <- parameters$nb.variables[v]
-        
-        ## For the time being we do this experiment only with log2 normalised counts 
-        ## since we saw that it improves the result with all variables  
+
+        ## For the time being we do this experiment only with log2 normalised counts
+        ## since we saw that it improves the result with all variables
         data.type <- "log2normCounts"
         data.table <- data.frame(get(data.type))
         selected.DEG.names <- DEG$geneOrder[1:varnb]
 
-        ## Make sure that we select gene names present in the selected data type 
+        ## Make sure that we select gene names present in the selected data type
         ## (some genes may be filtered out or technical reasons)
         valid.DEG.names <- selected.DEG.names[selected.DEG.names %in% colnames(data.table)]
-        
+
         counts <- data.table[,valid.DEG.names]
-        
-        
+
+
         ## dim(counts)
-        
+
         ## Define experiment prefix
         variable.type <- paste(sep="_", "DEG", deg.method, "top", varnb)
         exp.prefix <- paste(sep="_", classifier, data.type, variable.type)
@@ -55,8 +55,8 @@ if (parameters$compute) {
           exp.prefix <- paste(sep="_", exp.prefix, perm.prefix)
         }
         message (format(Sys.time(), "%Y-%m-%d_%H%M%S"), "\t", "Experiment prefix: ", exp.prefix)
-        
-        train.test.results.DEG[[exp.prefix]] <- 
+
+        train.test.results.DEG[[exp.prefix]] <-
           one.experiment(countTable=counts,
                          data.type=data.type,
                          classifier=classifier,
@@ -67,25 +67,25 @@ if (parameters$compute) {
                          file.prefix = exp.prefix)
       }
     }
-  }  
+  }
   #  } # end for the assiciate the analysis for each classifier
 }  else {
   message.with.time("Skipping train/test analysis with DEG-ordered top variables")
 }
 
 ## For each experiment, the results were stored in an element of the list train.test.results.
-## Test the list of experiments done. 
+## Test the list of experiments done.
 names(train.test.results.DEG)
 
 #################################################################################################################
 ### results for the imapct of the number of variables (genes) sorted according to DEG
 #################################################################################################################
-ErrorRateBoxPlot(experimentList = train.test.results.DEG, 
+ErrorRateBoxPlot(experimentList = train.test.results.DEG,
                  classifier=classifier,
                  variable.type = "ordered_variables_2",
-                 main = paste(  "imcapt of number of variables sorted according DEG", "\n", 
+                 main = paste(  "imcapt of number of variables sorted according DEG", "\n",
                                 parameters$iterations,
-                                "iterations", 
+                                "iterations",
                                 "log2normcounts" ,
                                 "ordered_variables"))
 
