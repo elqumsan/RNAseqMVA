1a363f014aa0399b7a8f2a469745b2ce3c4a5120
elqumsan <m.qumsan@gmail.com>
2017-11-28 18:00:42 +0100

    just testing other data set

--- misc/01_parameters_and_libraries.R
+++ misc/01_parameters_and_libraries.R
@@ -20,9 +20,9 @@ project.parameters <- yaml.load_file("misc/00_project_parameters.yml")
 ## Lad default parameters (must have been defined n the parameter field)
 parameters <- project.parameters$default
 
-# recountID <- "SRP042620" ## Multi-group breast cancer
+ recountID <- "SRP042620" ## Multi-group breast cancer
 #recountID <- "SRP003611" # transcriptomes of 347 cells from 10 distinct populations in both of low-coverage (~0.27 million reads per cell) and high-coverage (~5 million reads per cell)
-recountID <- "SRP061240"
+#recountID <- "SRP061240"
 parameters$recountID <- recountID
 
 ## Overwrite default parameters wih project-specific parameters
