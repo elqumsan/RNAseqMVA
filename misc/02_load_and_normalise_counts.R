60d2e077fa5263db0cf442638ded14dd2624c74c
elqumsan <m.qumsan@gmail.com>
2017-11-30 14:44:00 +0100

    adding guidance message

--- misc/02_load_and_normalise_counts.R
+++ misc/02_load_and_normalise_counts.R
@@ -125,3 +125,7 @@ message.with.time("Pre-processing by Principal Component analysis (PCA)")
 log2norm.prcomp.centred <- prcomp(log2normCounts, center = TRUE, scale. = FALSE)
 log2norm.prcomp.centred.scaled <- prcomp(log2normCounts, center = TRUE, scale. = TRUE)
 
+## Indicate that this principal components analysis for log2 count has finished running
+message.with.time("finished running Principal Component analysis (PCA) for normalized log2 counts")
+
+
