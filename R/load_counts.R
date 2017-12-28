0200e78790023f67dff8c7ec5f1e4f3f055056e4
elqumsan <m.qumsan@gmail.com>
2017-12-28 14:02:48 +0100

    chanhe the dir.workspace value

--- R/load_counts.R
+++ R/load_counts.R
@@ -46,7 +46,8 @@ loadCounts <- function(recountID = parameters$recountID,
   # loading count Data from recount_experiment, Via our wrapper which will Automatically merge the runs by
   # sample in order to obtain sample-wise count rather than run-wise counts.
 
-  countdata <- loadRecountExperiment(recountID=recountID, mergeRuns=mergeRuns, sampleIdColumn=sampleIdColumn, dir.workspace = dir.workspace, ...)
+  countdata <- loadRecountExperiment(recountID=recountID, mergeRuns=mergeRuns, sampleIdColumn=sampleIdColumn,
+                                     dir.workspace = parameters$dir$workspace, ...)
 
   ################################################################
   ## Tanspose the count tabe in oredr to use it with classifier methods
