9842763146936614bc95bb4b895fec059a22625b
elqumsan <m.qumsan@gmail.com>
2018-01-23 14:04:26 +0100

    creating new protocols for providing the instruction to load the data in a conventional format

---
+++ misc/protocol_loading_data_in_conventional_format.R
@@ -0,0 +1,11 @@
+#' @title a minimal protocol to providing the minimal instructions to load the data in a conventional format (tab-delimited, everything in the form of data.frames)
+#' @author  Mustafa ABUELQUMSAN and Jacques van Helden
+#' @description for the safe of facilitate the data exchanges with statisticians and biloigest we expoert all the RNAseq data in a conventional formate (tab-delimited, everything in the form of data.frames)
+
+#' @param
+#' @return
+#' @import
+#' @export
+
+
+# How to laod the raw count table
