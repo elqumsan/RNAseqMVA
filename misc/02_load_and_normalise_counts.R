f05e37589a9c9e0eab42d773b57c42029b4ce297
elqumsan <m.qumsan@gmail.com>
2018-02-06 12:50:22 +0100

    change the exhibit the geo characteristics

--- misc/02_load_and_normalise_counts.R
+++ misc/02_load_and_normalise_counts.R
@@ -130,3 +130,10 @@ if (parameters$compute) {
 ## Indicate that this script has finished running
 message.with.time("finished executing 02_load_and_normalise_counts.R")
 
+##### Exhibiting the geo charactiristics for the current project #####
+message.with.time("Exhibit the geo charactiristics for such experiment: ", parameters$recountID, " in order to know the class lable
+                  for such experiment")
+head( geo.characteristics)
+geo.characteristics.file <- file.path("~/RNAseqMVA_workspace", "data", parameters$recountID, "geo.characteristics.tsv")
+write.table( geo.characteristics, file = geo.characteristics.file, quote = FALSE,
+             row.names = FALSE, sep = "\t" )
--- misc/03_prcomp_computation.R
+++ misc/03_prcomp_computation.R
@@ -7,10 +7,4 @@ log2norm.prcomp.centred.scaled <- prcomp(na.omit(log2norm), center = TRUE, scale
 ## Indicate that this principal components analysis for log2 count has finished running
 message.with.time("finished running Principal Component analysis (PCA) for normalized log2 counts")
 
-##### Exhibiting the geo charactiristics for the current project #####
-message.with.time("Exhibit the geo charactiristics for such experiment: ", parameters$recountID, " in order to know the class lable
-                  for such experiment")
-head( geo.characteristics)
-geo.characteristics.file <- file.path("~/RNAseqMVA_workspace", "data", parameters$recountID, "geo.characteristics.tsv")
-write.table( geo.characteristics, file = geo.characteristics.file, quote = FALSE,
-             row.names = FALSE, sep = "\t" )
+
