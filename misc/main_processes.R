2c965b33c9116231136197fda8f6f3d910dfc6a6
elqumsan <m.qumsan@gmail.com>
2018-02-13 11:46:48 +0100

    some modification of the main processes

---
+++ misc/main_processes.R
@@ -0,0 +1,9 @@
+if(parameters$compute){
+
+  source("~/RNAseqMVA/misc/07_PCA_impact_of_PC_number.R")
+  source("~/RNAseqMVA/misc/08_DEG_impact_of_top_gene_number.R")
+  source("~/RNAseqMVA/misc/09_Importance_Variables_impact_of_top_genes.R")
+
+
+
+}
