5fc475d1a7dc3dc4b7a3a686c043ee1b787c4104
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-03-02 12:51:22 +0100

    Entirely revised by Jacques and Mustafa

--- R/load_counts.R
+++ R/load_counts.R
@@ -9,7 +9,7 @@
 #' @param classColumn ## Column(s) of the pheno table used to define class labels. Passed to filterCountTable()
 #' @param minSamplesPerClass=parameters$minSamplesPerClass min number of samples per class for the filtering.  Passed to filterCountTable()
 #' @param dir.workspace=parameters$dir$workspace Directory in which the data and results will be stored.
-#' @param ...  additional parameters are passed to loadRecountExperiment
+#' @param ...  additional parameters are passed to loadRecountExperiment()
 #'
 #' @return
 #' @examples
@@ -36,6 +36,7 @@ loadCounts <- function(recountID = parameters$recountID,
                        minSamplesPerClass = parameters$minSamplesPerClass,
                        dir.workspace = parameters$dir$workspace,
                        ... ) {
+  message.with.time("Starting loadCounts() for Recount ID ", parameters$recountID)
 
   ################################################
   # loading required libraries and install them if required
@@ -46,77 +47,82 @@ loadCounts <- function(recountID = parameters$recountID,
   # loading count Data from recount_experiment, Via our wrapper which will Automatically merge the runs by
   # sample in order to obtain sample-wise count rather than run-wise counts.
 
-  countdata <- loadRecountExperiment(recountID=recountID, mergeRuns=mergeRuns, sampleIdColumn=parameters$sampleIdColumn,
-                                     dir.workspace = parameters$dir$workspace, ...)
+  countdata <- loadRecountExperiment(recountID=recountID,
+                                     mergeRuns=mergeRuns,
+                                     sampleIdColumn=parameters$sampleIdColumn,
+                                     dir.workspace = parameters$dir$workspace,
+                                     ...)
 
   ################################################################
-  ## Tanspose the count tabe in oredr to use it with classifier methods
-  ## which expect a table with one raw per observation (biological samples)
-  ## and one column per variable (gene).
-  message("Transpose the count table to use it with classifier methods ")
-  if(mergeRuns == 1){
-  countTable <-as.data.frame(t(countdata$merged$sampleCounts))
+  ## Tanspose the count tabe in order to use it with classifier methods
+  ## which expect a table with one raw per individual (biological samples)
+  ## and one column per feature (gene).
+  message("Transposing count table to use it with classifier methods. ")
+  if (mergeRuns){
+    countTable <-as.data.frame(t(countdata$merged$sampleCounts))
   } else {
     countTable <- as.data.frame(t(countdata$countsPerRun))
   }
+  message("\tcountTable contains ",
+          nrow(countTable), " rows (samples) and ",
+          ncol(countTable), " columns (genes).")
   #countTable <-as.data.frame(t(countdata$runCounts))
 
 
   ################################################################
   # Extract pheno table for the sample-wised merged count
   message("Extracting pheno Table from sample-wised merged count")
-  if(mergeRuns == 1){
-   phenoTable <- countdata$merged$samplePheno
+  if (mergeRuns){
+    phenoTable <- countdata$merged$samplePheno
   } else {
     phenoTable <- countdata$runPhenoTable
   }
+  message("\tphenoTable contains ",
+          nrow(phenoTable), " rows (samples) and ",
+          ncol(phenoTable), " columns (pheno description fields).")
+  # names(phenoTable)
+
   #phenoTable <- countdata$runPheno$characteristics
-  countsPerRun <- countdata$countsPerRun
+  #countsPerRun <- countdata$countsPerRun
   geo.characteristics <- countdata$geo.characteristics
   #dim(phenoTable)
 
   ################################################
   ## Filter zero-variance and near-zero variance variables from the count table
-  filteredData <- filterCountTable(countTable , phenoTable,
-                                   classColumn =parameters$classColumn,
-                                   minSamplesPerClass = parameters$minSamplesPerClass)
+  filteredData <- filterCountTable(countTable = countTable,
+                                   phenoTable = phenoTable,
+                                   classColumn = classColumn,
+                                   minSamplesPerClass = minSamplesPerClass)
 
   # replace unfiltered Data with filted data
   countTable <- filteredData$countTable
   phenoTable <- filteredData$phenoTable
-
-  classes <-make.names(filteredData$classes , unique = F , allow_ = T)
+  classes <- make.names(filteredData$classes , unique = F , allow_ = T)
   #classifier requires a factor for classes
-  classes <-as.factor(classes)
+  # classes <- as.factor(classes) ## RandomForest requires as.factor()
 
-  message("Count table dimensions: ", nrow(countTable), sep= " x ", ncol(countTable),"\n",
-          "Pheno table dimensions: ",nrow(phenoTable), " X ", ncol(phenoTable), "\n",
-          "Vector of class labels length: ", length(classes))
-  if (length(classes) != nrow(countTable) )
+  message("\tCount table dimensions: ", nrow(countTable), sep= " x ", ncol(countTable),
+          "\n\tPheno table dimensions: ",nrow(phenoTable), " X ", ncol(phenoTable),
+          "\n\tVector of class labels length: ", length(classes),
+          "\n\tNumber of distinct classes: ", length(unique(classes)))
+
+  if (length(classes) != nrow(countTable)) {
     stop("invaled number of classes labes, (", length(classes) ,"). ",
          "must equal the number of individuals in Count Table (",nrow(countTable),").")
+  }
 
-######### sptiting the countTable for the train set and test set #########
-  n <- nrow(countTable) ## Number of observations (samples)
-  train.size <- round(n * parameters$trainingProportion)
 
-  ## Random selection of indices for the training set
-  trainIndex <- sort(sample(1:n, size=train.size))
-  ## Use remaining indices for the testing set
-  testIndex <- setdiff(1:n, trainIndex)
 
-########### results is  loadedRecount ###########
+  #### Build a list with the results of the loadCounts() function
   loadedRecount <- list()
   loadedRecount$countTable<- countTable
   loadedRecount$phenoTable <- phenoTable
   loadedRecount$classes <- classes
-  loadedRecount$countsPerRuns <- countsPerRun
-  loadedRecount$runPheno <- countdata$runPheno
   loadedRecount$samples.per.class <- as.data.frame.table(table(classes), row.names=1)
   loadedRecount$filteredData <- filteredData
   loadedRecount$geo.characteristics <- geo.characteristics
-  loadedRecount$trainIndex <- trainIndex
-  loadedRecount$testIndex <- testIndex
+  loadedRecount$countsPerRuns <- countdata$countsPerRun
+  loadedRecount$runPheno <- countdata$runPheno
 
   message.with.time("Finished Load Count Table process for Recount experiment ID ", parameters$recountID)
 
