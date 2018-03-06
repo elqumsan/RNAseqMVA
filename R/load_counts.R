2f505e3ea0eddf63967cd225ea2f967eac04c354
elqumsan <m.qumsan@gmail.com>
2018-03-06 14:53:59 +0100

    minor changes

--- R/load_counts.R
+++ R/load_counts.R
@@ -132,7 +132,7 @@ loadCounts <- function(recountID = parameters$recountID,
   experiment$filteredClasses <- filteredData$filteredClasses
 
 
-  experiment$samples.per.class <- as.data.frame.table(table(classes), row.names=1)
+  experiment$samples.per.class <- as.data.frame.table(table(filteredData$classes), row.names=1)
   experiment$filteredData <- filteredData
   experiment$geo.characteristics <- geo.characteristics
   experiment$countsPerRuns <- experiment$countsPerRun
