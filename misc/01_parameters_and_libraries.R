4310648ba98df3baf0c53c31ab65c9b7861c7150
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-03 20:35:10 +0200

    moved required librairies

--- misc/01_parameters_and_libraries.R
+++ misc/01_parameters_and_libraries.R
@@ -1,24 +1,36 @@
+message.with.time("Loading required libraries")
 require("roxygen2")
 require("devtools")
 require("RNAseqMVA")
 require("yaml")
 
+# loading required libraries
+requiredCRAN <- c('devtools', 'class', "randomForest","broom", "roxygen2","scatterplot3d","e1071")
+RequiredCRANPackages(requiredCRAN)
+
+## JvH: Mustafa, please add the other required packages, in particular recount
+requiredBioconductor <- c("recount")
+RequiredBioconductorPackages(requiredBioconductor)
+
+yaml.file <- "misc/00_project_parameters.yml"
+message.with.time("Loading parameters from YAM file ", yaml.file)
+
 ## Read project-specific parameters from a yaml-formatted file.
 ## These parameters will then be used to overwrite the default parameters.
-project.parameters <- yaml.load_file("misc/00_project_parameters.yml")
+project.parameters <- yaml.load_file(yaml.file)
 
 ## Lad default parameters (must have been defined n the parameter field)
 parameters <- project.parameters$default
 
 ## Get all recount IDs
 recountIDs <- grep(pattern = "^SRP", x = names(project.parameters), value = TRUE)
-message("YAML config file contains ", length(recountIDs)," recount IDs.")
+message("\tYAML config file contains ", length(recountIDs)," recount IDs.")
 selectedRecountIDs <- c("SRP042620", "SRP057196")
-message("Selected ", length(selectedRecountIDs)," recount IDs: ", paste(collapse="; ", selectedRecountIDs))
+message("\tSelected ", length(selectedRecountIDs)," recount IDs: ", paste(collapse="; ", selectedRecountIDs))
 #selectedRecountIDs <- recountIDs
 
 recountID <- "SRP042620"   ## Multi-group breast cancer
-message("Default recountID ", recountID)
+message("\tDefault recountID ", recountID)
 # recountID <- "SRP057196"    # individual cells into all of the major neuronal, glial, and vascular cell types in the brain
 
 # recountID <- "SRP003611"   # transcriptomes of 347 cells from 10 distinct populations in both of low-coverage (~0.27 million reads per cell) and high-coverage (~5 million reads per cell)
@@ -43,37 +55,6 @@ message("Default recountID ", recountID)
 # recountID <-  "SRP039694"  # Not working: hepatocellular carcinoma
 # recountID <- "SRP008976"   # NOT working properly
 # recountID <- "SRP006575"   # Not working
-
-# View(parameters)
-
-#
-#
-# ## Parameters for knn
-# parameters$knn = list(k=10) # the number of neighbours considered
-#
-# ## Parameters for svm
-# parameters$svm = list(
-#   kernel="linear",
-#   scale=FALSE,
-#   type="C-classification" ,
-#   gamma = 1,
-#   cost = 100)
-
-
-## Temporary, to get quick results
-# parameters$classifiers <- c("knn")
-# parameters$iterations <- 3
-
-
-
-# loading required libraries
-requiredCRAN <- c('devtools', 'class', "randomForest","broom", "roxygen2","scatterplot3d","e1071")
-RequiredCRANPackages(requiredCRAN)
-
-## JvH: Mustafa, please add the other required packages, in particular recount
-requiredBioconductor <- c("recount")
-RequiredBioconductorPackages(requiredBioconductor)
-
 
 ## END OF SCRIPT
 #################################################################
