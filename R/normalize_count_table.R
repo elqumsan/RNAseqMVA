4efac7c10d45b2331c5cc3c2fc9d02796f3a218b
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-05 22:19:12 +0200

    still ongoing: fixing bugs occurring when samples are discarded during the normalisation procedure due to scaling factor == 0

--- R/normalize_count_table.R
+++ R/normalize_count_table.R
@@ -1,9 +1,15 @@
 #' @title Normalize RNA-seq count table
 #' @author Jacques van Helden and Mustafa AbuElqumsan
-#' @description Normalize a table of raw counts from RNA-seq
-#' @param counts a raw count table, with one column per sample and one row per gene.
-#' Normalization will be done by samples (columns).
-#' @param phenoTable the pheno table associated to the count table. Should be provided in order to adapt it in case samples would be suppressed because they have a scaling factor of 0.
+#' @description normalisation of RNA-seq count table.
+#' More precisely this function runs a sample-wise scaling so that all the samples
+#' have the same value for auser-defined scaling parameter.
+#' By default, we use the quantile 0.75 as scaling factor.
+#'
+#' @param self an object belonging to the class countTableWithClasses
+#' This input object contains a count table, with one column per sample and
+#' one row per gene + a phenoTable + sample class specifications.
+#'
+#' The phenoTable of the input object may be filtered out adapted it in case samples would be suppressed because they have a scaling factor of 0.
 #' @param classLabels the class labels associated each sample of the count table. Should be provided in order to adapt it in case samples would be suppressed because they have a scaling factor of 0.
 #'
 #' @param method="quantile" normalization method. Supported: sum, mean, media, quantile.
@@ -18,52 +24,52 @@
 #' @param epsilon=0.1 value added to all counts before applying the log2 transformation
 #' in order to avoid zero counts.
 #'
-#' @return a list with the normalized count table + a table providing sample-wise statistics
+#' @return the function returns an object of class countTableWithClasses, where the countTable
+#' coontains the normalised counts. Note that the normalized count table may have a smaller
+#' number of samples than the input count table because if some samples have a scaling
+#' factor of 0 they will be filtered out. In such case, the phenoTable and classLabels will
+#' be filtered out in the same way to ensure consistency  of the attributes of the
+#' returned countTableWithClass object.
+#'
 #'
 #' @examples
 #'
 #' @export
 NormalizeCounts <- function(self,
-                            # counts,
-                            #phenoTable,
-                            #classLabels,
                             classColumn = parameters$classColumn,
-                            classColors = parameters$classColor,
+                            classColors = parameters$classColors,
                             method="quantile",
                             quantile=0.75,
                             log2=TRUE,
                             epsilon=0.1) {
 
-  if(log2){
+  if (log2) {
     message.with.time("Starting log2 NormalizeCounts() for Recount experiment ID ", self[["ID"]])
-
-
   } else{
-
     message.with.time("Starting NormalizeCounts() for Recount experiment ID ", self[["ID"]])
-
   }
 
-  counts <- self$countTable
-  phenoTable <- self$phenoTable
 
-  classLabels  <- self$classLabels
+  # counts <- self$countTable
+  # phenoTable <- self$phenoTable
+  # classLabels  <- self$classLabels
 
 
-  #dim(counts)
+  #dim(self$countTable)
   ## Compute sample-wise statistics
+  message("\t", "Computing sample-wise statistics\t", recountID)
   sampleStats <- data.frame(
-    sum = apply(counts, 2, sum, na.rm=TRUE),
-    min = apply(counts, 2, min, na.rm=TRUE),
-    mean = apply(counts, 2, mean, na.rm=TRUE),
-    Q1 = apply(counts, 2, quantile, na.rm=TRUE, probs=0.25),
-    median = apply(counts, 2, median, na.rm=TRUE),
-    Q3 = apply(counts, 2, quantile, na.rm=TRUE, probs=0.75),
-    max = apply(counts, 2, max, na.rm=TRUE),
-    zero.values = apply(counts == 0, 2, sum),
-    na.values = apply(is.na(counts), 2, sum),
-   # infinite.values = apply(is.infinite(counts), 2, sum)
-   infinite.values = length(is.infinite(unlist(counts)) == TRUE)
+    sum = apply(self$countTable, 2, sum, na.rm=TRUE),
+    min = apply(self$countTable, 2, min, na.rm=TRUE),
+    mean = apply(self$countTable, 2, mean, na.rm=TRUE),
+    Q1 = apply(self$countTable, 2, quantile, na.rm=TRUE, probs=0.25),
+    median = apply(self$countTable, 2, median, na.rm=TRUE),
+    Q3 = apply(self$countTable, 2, quantile, na.rm=TRUE, probs=0.75),
+    max = apply(self$countTable, 2, max, na.rm=TRUE),
+    zero.values = apply(self$countTable == 0, 2, sum),
+    na.values = apply(is.na(self$countTable), 2, sum),
+   # infinite.values = apply(is.infinite(self$countTable), 2, sum)
+   infinite.values = length(is.infinite(unlist(self$countTable)) == TRUE)
   )
 
   ## check it the original data contains NA values
@@ -72,15 +78,19 @@ NormalizeCounts <- function(self,
   }
 
   if (method == "quantile") {
-    sampleStats$norm.quantile <- apply(counts, 2, quantile, na.rm=TRUE, probs=quantile)
+    message("\tNormalizing counts. Scaling factor: sample quantile ", quantile)
+    sampleStats$norm.quantile <- apply(self$countTable, 2, quantile, na.rm=TRUE, probs=quantile)
     sampleStats$scaling.factor <- sampleStats$norm.quantile
   } else if (method == "mean") {
+    message("\tNormalizing counts. Scaling factor: sample mean.  ")
     ## Note: mean is very sensitive to outliers, which are very problematic with RNAseq data
-  #  sampleStats$scaling.factor <- apply(counts, 2, quantile, na.rm=TRUE, probs=quantile)
+  #  sampleStats$scaling.factor <- apply(self$countTable, 2, quantile, na.rm=TRUE, probs=quantile)
     sampleStats$scaling.factor <- sampleStats$mean
   } else if (method == "median") {
+    message("\tNormalizing counts. Scaling factor: sample median.  ")
     sampleStats$scaling.factor <- sampleStats$median
   } else if (method == "sum") {
+    message("\tNormalizing counts. Scaling factor: sample sum (= libsum)  ")
     sampleStats$scaling.factor <- sampleStats$sum
   } else {
     stop(method, " is not a valid method for NormalizeCounts()")
@@ -88,18 +98,21 @@ NormalizeCounts <- function(self,
 
   ## Detect problems related to null scaling factors, which may happen in some datasets due to a very large number of zeros.
   zeroScaledSamples <- sampleStats$scaling.factor == 0
-  discaredSampleNames <- colnames(counts[, zeroScaledSamples])
+  discaredSampleNames <- colnames(self$countTable[, zeroScaledSamples])
 
-  # apply(counts[, zeroScaledSamples]==0, 2, sum)
+  # apply(self$countTable[, zeroScaledSamples]==0, 2, sum)
   message("\tDiscarding ", sum(zeroScaledSamples), " samples because their scaling factor is null. ")
   message("\tDiscarded samples: ", paste(collapse="; ", discaredSampleNames))
 
   ## Compute normalised counts
   normTarget <- median(sampleStats$scaling.factor[!zeroScaledSamples])
-  normCounts <- t(t(counts[, !zeroScaledSamples]) / sampleStats$scaling.factor[!zeroScaledSamples]) * normTarget
+  normCounts <- t(t(self$countTable[, !zeroScaledSamples]) / sampleStats$scaling.factor[!zeroScaledSamples]) * normTarget
   # Check that all normalised coutns have the same scaling factor
   # apply(normCounts, 2, quantile, probs=quantile)
-
+  # dim(self$countTable)
+  # dim(self$phenoTable)
+  # length(self$classLabels)
+  # dim(normCounts)
 
   ## Run log2 transformation if required
   if (log2) {
@@ -128,6 +141,19 @@ NormalizeCounts <- function(self,
 
   result <- self
   result$countTable <- normCounts
+  if (sum(zeroScaledSamples) > 0) {
+    result$phenoTable <- self$phenoTable[!zeroScaledSamples,]
+
+    ## Update  sample-related parameters
+    result$sampleNames <- self$sampleNames[!zeroScaledSamples]
+    result$nbSamples <- length(result$sampleNames)
+
+    ## Update class-relatd parameters
+    defineSampleClasses(result)
+
+    ## TO DO !!!!
+    ## AFTER HAVING DISCARDD SAMPLES, WE NEED TO CHECK THAT THE NB OF SAMPLES PER CLAS IS STILL ABOVE THE THRESHOLD
+  }
 
   # hist(unlist(normCounts), breaks=100)
 
