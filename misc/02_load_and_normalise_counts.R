dd30ddd182100837509ac96b8d33e11c690152c6
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-07 18:21:28 +0200

    moved parameters to init_recount_id.R

--- misc/02_load_and_normalise_counts.R
+++ misc/02_load_and_normalise_counts.R
@@ -7,32 +7,32 @@ loaded <- list() ## a list containing all the loaded datasets + their pre-proces
 for (recountID in selectedRecountIDs) {
 
   #### Specify generic and recountID-specific parameters ####
-
-  ## Load default parameters for each new recountID
-  ## (was previously parsed from the YAML file)
-  parameters <- project.parameters$default
-
-  ## Specify the current recountID in parameters
-  parameters$recountID <- recountID
-
-  ## Overwrite default parameters wih project-specific parameters
-  selected.parameters <- project.parameters[[recountID]]
-  if (is.null(selected.parameters)) {
-    message("No specific parameters for recount ID ", recountID)
-    message("Using generic parameters from the yaml file. ")
-  } else {
-    message("Using specific parameters specfied in yaml file for recount ID ", recountID)
-    parameters[names(selected.parameters)] <- project.parameters[[recountID]]
-    names(parameters$data.types)<-parameters$data.types
-    names(parameters$variables.type)<-parameters$variables.type
-  }
-
-  ## Convert list-formatted class colors to named vector (YAML does not allow to specify named vectors)
-  if (!is.null(parameters$classColors)) {
-    if (class(parameters$classColors) == "list") {
-      parameters$classColors <- unlist(parameters$classColors)
-    }
-  }
+  parameters <- initRecountID(recountID, configFile)
+  # ## Load default parameters for each new recountID
+  # ## (was previously parsed from the YAML file)
+  # parameters <- project.parameters$default
+  #
+  # ## Specify the current recountID in parameters
+  # parameters$recountID <- recountID
+  #
+  # ## Overwrite default parameters wih project-specific parameters
+  # selected.parameters <- project.parameters[[recountID]]
+  # if (is.null(selected.parameters)) {
+  #   message("No specific parameters for recount ID ", recountID)
+  #   message("Using generic parameters from the yaml file. ")
+  # } else {
+  #   message("Using specific parameters specfied in yaml file for recount ID ", recountID)
+  #   parameters[names(selected.parameters)] <- project.parameters[[recountID]]
+  #   names(parameters$data.types)<-parameters$data.types
+  #   names(parameters$variables.type)<-parameters$variables.type
+  # }
+  #
+  # ## Convert list-formatted class colors to named vector (YAML does not allow to specify named vectors)
+  # if (!is.null(parameters$classColors)) {
+  #   if (class(parameters$classColors) == "list") {
+  #     parameters$classColors <- unlist(parameters$classColors)
+  #   }
+  # }
 
   ## BEWARE: NOT SURE THIS IS FUNCTIONAL, RELOADING MEMORY IMAGES WILL BE TREATED LATER
   # if (parameters$reload == TRUE) {
