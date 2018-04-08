4754ca6f958456cdbf227963744b7586195a507d
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-08 15:14:05 +0200

    implemented exportTables.countTableWithTrainTestSets for a class-specific export of train and test indices

--- R/exportTables.R
+++ R/exportTables.R
@@ -3,11 +3,20 @@
 #' @author Mustafa AbuElQumsan and Jacques van Helden
 #' @param self an object, which must belong to a compatible class
 #' @export
- exportTables <- function (self, ...) {
-   message("Exporting object of class ", class(self), " to tables")
-   #  message("Looking for a function named ", paste("exportTables", class(self), sep="."))
-   UseMethod("exportTables", self)
- }
+exportTables <- function (self, ...) {
+  message("Exporting object of class ", class(self), " to tables")
+  #  message("Looking for a function named ", paste("exportTables", class(self), sep="."))
+  UseMethod("exportTables", self)
+}
+
+#' @title default export table (for closing the NextMethod calls)
+#' @author Jacques van Helden
+#' @export
+exportTables.default <- function (self, ...) {
+  message("Finished exporting tables")
+  #  message("Looking for a function named ", paste("exportTables", class(self), sep="."))
+  return()
+}
 
 
 
@@ -16,32 +25,69 @@
 #' @param self an object, which must belong to a compatible class
 #' @param export.dir export directory
 #' @param file.prefix file prefix to build the different tables
-#' @param extension=".tsv" extension for the expodrted files (tab-separated value)
+#' @param extension=".tsv" extension for the exported files (tab-separated values)
 #' @export
 exportTables.countTableWithClasses <- function (self,
                                                 export.dir,
                                                 file.prefix,
                                                 extension=".tsv") {
-  message("\tExporting countTableWithClasses object ", self[["ID"]], " to tables")
+  message("\tExporting countTableWithClasses object ", self$ID, " to tables")
   message("\t\tExport directory\t", export.dir)
+  dir.create(export.dir, showWarnings = FALSE, recursive = TRUE)
+
   message("\t\tFile prefix\t", file.prefix)
- # UseMethod("exportTables", self)
+  # UseMethod("exportTables", self)
 
   ############## Exporting the count table ####################
-  count.file <- file.path(export.dir, paste(file.prefix, self[["ID"]], "_count_table", extension, sep = ""))
+  count.file <- file.path(export.dir, paste(file.prefix, self$ID, "_count_table.tsv", extension, sep = ""))
   message("\t\tExporting count table in TSV file\t", count.file)
   write.table(self$countTable, file = count.file, row.names = FALSE, quote=FALSE, sep = "\t")
 
   ############## Exporting the  Pheno Table ####################
-  pheno.file <- file.path(export.dir, paste(file.prefix, self[["ID"]], "_pheno_table", extension, sep = ""))
+  pheno.file <- file.path(export.dir, paste(file.prefix, self$ID, "_pheno_table.tsv", extension, sep = ""))
   message("\t\tExporting  pheno table in TSV file\t", pheno.file)
   write.table(self$phenoTable, file = pheno.file, row.names = FALSE, quote=FALSE, sep = "\t")
 
   ############## Exporting the class labels ####################
-  classLabel.file <- file.path(export.dir, paste(file.prefix, self[["ID"]], "_class_labels", extension, sep = ""))
+  classLabel.file <- file.path(export.dir, paste(file.prefix, self$ID, "_class_labels.tsv", extension, sep = ""))
   message("\t\tExporting class labels in TSV file\t", classLabel.file)
   write.table(data.frame(sampleName = self$sampleNames, classLabel = self$classLabels),
               file = classLabel.file, row.names = FALSE, quote=FALSE, sep = "\t")
 
+  NextMethod("exportTables", self)
 }
+
+#' @title Export the tables specific to the class countTableWithTrainTestSets
+#' @author Jacques van Helden
+#' @param self an object, which must belong to class countTableWithTrainTestSets
+#' @param export.dir export directory
+#' @param file.prefix file prefix to build the different tables
+#' @param extension=".tsv" extension for the exported files (tab-separated values)
+#' @export
+exportTables.countTableWithTrainTestSets <- function (self,
+                                                export.dir,
+                                                file.prefix,
+                                                extension=".tsv") {
+  message("\tExporting countTableWithTrainTestSets object ", self$ID, " to tables")
+  message("\t\tExport directory\t", export.dir)
+  dir.create(export.dir, showWarnings = FALSE, recursive = TRUE)
+
+  message("\t\tFile prefix\t", file.prefix)
+  # UseMethod("exportTables", self)
+
+  ############## Exporting train and test indices ####################
+  trainIndices.file <- file.path(export.dir, paste(file.prefix, self$ID, "_trainindices.tsv", extension, sep = ""))
+  message("\t\tExporting count table in TSV file\t", count.file)
+  write.table(as.data.frame(self$trainTestProperties$trainIndices), file = trainIndices.file, row.names = FALSE, col.names = FALSE, quote=FALSE, sep = "\t")
+
+  testIndices.file <- file.path(export.dir, paste(file.prefix, self$ID, "_testindices.tsv", extension, sep = ""))
+  message("\t\tExporting count table in TSV file\t", count.file)
+  write.table(as.data.frame(self$testTestProperties$testIndices), file = testIndices.file, row.names = FALSE, col.names = FALSE, quote=FALSE, sep = "\t")
+
+
+  NextMethod("exportTables", self)
+}
+
+
+
 
