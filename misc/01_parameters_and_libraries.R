a019a207abcaf162f9d1a2627ba41cfd6dffdf1e
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-09 06:47:17 +0200

    initialise a "cluster" for parallel computing with packages foreach and doParallel

--- misc/01_parameters_and_libraries.R
+++ misc/01_parameters_and_libraries.R
@@ -82,9 +82,42 @@ recountID <- "SRP057196"    # individual cells into all of the major neuronal, g
 
 message("\tDefault recountID ", recountID)
 
-################################################################
-## Define general directories.
-## Dataset-specific directories are defined later.
+#### Initialise cluster processing ####
+if (!is.null(project.parameters$default$jobs)) {
+  message("\tSetting parallelisation parameters")
+  require(foreach)
+  require(doParallel)
+
+  ## Define the max number of parallel jobs
+  no_cores <- NULL
+  jobs <- project.parameters$default$jobs
+  if (is.integer(jobs) && (jobs > 0)) {
+    no_cores <- jobs
+  } else if (jobs == "auto") {
+    no_cores <- as.integer(detectCores() - 1) ## Define the number of cores (keep one for the system)
+  } else if (jobs == "none") {
+    message("Parallel computing not activated in config file. ")
+  } else {
+    stop(jobs, ' is not a valid value for the number of jobs. Should be either a strictly positive integer, or "auto".')
+  }
+
+  if (is.integer(no_cores) && (no_cores > 0)) {
+    message("\tInitializing cluster computing with max ", no_cores, " parallel jobs. ")
+    cl <- makeCluster(no_cores) ## Instantiate the cluser
+    passed.libs <- clusterEvalQ(cl, library(RNAseqMVA)) ## pass the RNAseqMVA library to the cluster
+    # clusterExport(cl, "project.parameters") ## pass the global variable "parameters" to the cluster
+    registerDoParallel(cl)   ## Register the cluster for parallel computing
+    project.parameters$default$parallel <- TRUE
+    project.parameters$no_cores <- no_cores
+  } else {
+    project.parameters$default$parallel <- FALSE
+  }
+
+  ## NOTE: WE SHOULD HAVE A TERMINATING SCRIPT, FOR INSTANCE TO STOP THE CLUSTER
+  ## Stop the cluster
+  #  stopImplicitCluster()
+
+}
 
 
 ## END OF SCRIPT
