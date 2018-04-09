66b03b00bf57a59e76cb92b054f881a57a2de55f
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-09 15:23:14 +0200

    removed classColors from required parameters

--- R/load_recount_experiment.R
+++ R/load_recount_experiment.R
@@ -54,7 +54,7 @@ loadRecountExperiment <- function(recountID = parameters$recountID,
   message.with.time("loadRecountExperiment()\trecountID = ", recountID)
 
   ## Check required parameters
-  for (p in c("mergeRuns", "sampleIdColumn", "classColumn", "classColors", "verbose", "minSamplesPerClass", "na.rm", "studyPath")) {
+  for (p in c("mergeRuns", "sampleIdColumn", "classColumn", "verbose", "minSamplesPerClass", "na.rm", "studyPath")) {
     if (is.null(parameters[[p]])) {
       stop("Missing required parameter: '", p,
            "'.\n\tPlease check configuration file. ")
