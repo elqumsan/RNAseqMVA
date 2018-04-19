942e76b88beaf16670cacaab89005c30d555b57e
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-04-19 18:55:01 +0200

    tund the code output dir

--- R/file_prefix.R
+++ R/file_prefix.R
@@ -1,46 +1,80 @@
-#' @title file prefix procedures with a given classifier and a given object
+#' @title return output parameters (output directory, file prefix, figure label)  for a given analysis given its parameterrs (dataset, classifier, permutation or not)
 #' @author Mustafa ABUELQUMSAN and Jacques van Helden
-#' @description due to such file will be called from other scripts in order to computes some classifier-specific parameters
-#' @param self an object of class DataTableWithTrainTestSets
+#' @param dataset an object of class DataTableWithTrainTestSets
 #' @param classifier is the type of classifier that is used with repeated process.
-#' @param permute is show if the class lable are permuted this for sake of the knowing the strength and weaknesses of the classifier
-#' @param file.prefix in order to let us to save file for any other script
+#' @param permute=FALSE is show if the class lable are permuted this for sake of the knowing the strength and weaknesses of the classifier
+#' @param createDir=TRUE if TRUE, the result directory is created automatically if it does not exist
 #'
-#' @return
-#' @import foreach
-#' @import doParallel
+#' @return a list with the following fields
+#' \itemize {
+#'   \item resultDir. path to the result directory, in which teh result files should be stored.
+#'   \item filePrefix. string that should prepend the name of each result file.
+#'   \item figureLabel.  short string with a label for the graphics
+#' }
 #' @export
+outputParameters <- function(dataset,
+                             classifier,
+                             permute = FALSE,
+                             createDir = TRUE) {
 
-
-filePrefix <- function( self,
-                        classifier,
-                        permute = FALSE,
-                        file.prefix = NULL){
-
-  message("\tDefine the file Prefix for each classifier for saking study the impact of ",classifier," parameters:")
-  ## TO DO:THIS SHOULD BE MOVED TO A SEPARATE FUNCTION
-  ## Define file prefix is not specified in paramters
-  if (is.null(file.prefix)) {
-    if (classifier == "svm") {
-      if (is.null(parameters$svm$kernel)) {
-        parameters$svm$kernel = "linear"
-      }
-      classifier_prefix = paste(sep="", "svm_", self$parameters$svm$kernel)
-    } else if (classifier == "knn") {
-      if (is.null(parameters$knn$k)) {
-        parameters$knn$k
-      }
-      classifier_prefix = paste(sep="", "knn_", self$parameters$knn$k)
-
-    } else {
-      classifier_prefix = classifier
-    }
-    file.prefix <- paste(sep="_", classifier_prefix,  self$dataType, self$variablesType)
-    file.prefix <- sub(pattern = " ", replacement = "_", x = file.prefix) ## Avoid spaces in file names
-    if (permute) {
-      file.prefix <- paste( file.prefix, "permLabels")
-    }
+  ## Check the class of hte dataset
+  if (!is(object = dataset, class2 = "DataTableWithTrainTestSets")) {
+    stop("filePrefix requires an object of class DataTableWithTrainTestSets")
   }
 
-return(file.prefix)
+  ##  Verbosity
+  message("\tDefining file Prefix for dataset ", dataset$ID,
+          "; data type: ", dataset$dataType,
+          "; classifier: ", classifier)
+
+  ## Define the piece of prefix and output dir that indicates the classifier
+
+  ## For some classifiers, the file prefix includes the main parameters
+  if (classifier == "svm") {
+    ## For SVM, the prefix includes the kernel
+    if (is.null(dataset$parameters$svm$kernel)) {
+      dataset$parameters$svm$kernel = "linear"
+    }
+    classifier_prefix = paste(sep="", "svm_", dataset$parameters$svm$kernel)
+  } else if (classifier == "knn") {
+    ## For KNN, the prefix includes the value of the k parameter
+    if (is.null(dataset$parameters$knn$k)) {
+      dataset$parameters$knn$k
+    }
+    classifier_prefix = paste(sep="", "knn_k", dataset$parameters$knn$k)
+
+  } else {
+    ## For all other types of calssifer, the variable
+    ## classifier_prefix is simply the name of the classifier
+    classifier_prefix = classifier
+  }
+
+
+  ## Define the path to the result directory
+  resultDir <- file.path(dataset$parameters$dir$results,
+                         dataset$ID,
+                         classifier)
+
+  dir.create(outParam$resultDir, showWarnings = FALSE, recursive = TRUE)
+
+  ## Define file prefix
+  filePrefix <- paste(sep="_", dataset$ID, classifier_prefix,  dataset$dataType)
+  if (permute) {
+    filePrefix <- paste(sep="_", filePrefix, dataset$parameters$perm.prefix)
+  }
+  filePrefix <- sub(pattern = " ", replacement = "_", x = filePrefix) ## Avoid spaces in file names
+
+  ## Define file label for figures
+  figLabel <- paste(sep=" ", classifier_prefix,  dataset$dataType)
+  if (permute) {
+    figLabel <- paste(sep=" ", figLabel, dataset$parameters$perm.prefix)
+  }
+  figLabel <- sub(pattern = "_", replacement = " ", x = figLabel) ## For figure labels spaces are more readable than underscores
+
+
+
+
+  return(list("resultDir" = resultDir,
+              "filePrexif" = filePrefix,
+              "figLabel" = figLabel))
 }
