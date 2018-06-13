54ec3bcc38dafa053c77784a9262ae26e7da5572
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-06-13 12:24:23 +0200

    majour modification to adapting error rate box plot for to genrate nice figures

--- R/error_rate_box_plot.R
+++ R/error_rate_box_plot.R
@@ -20,109 +20,111 @@
 # and draw a box plot
 ##################################################################################
 #' @export
-ErrorRateBoxPlot <- function(experimentList,
-                             classifier,
-                             data.type, # = "log2norm.prcomp.centred",
-#                             variablesType,
-                             main = paste(sep="", parameters$recountID, "; ", classifier,
-                                          "\n", data.type,
-                                          #"; ", variablesType,
-                                          "; ", parameters$iterations, "iterations"),
-                             expColor = "#00BBFF",
-                             permColor = "grey",
-                             expLegend = "Train/test",
-                             permLegend = "Permuted labels",
-                             ....) {
+ErrorRateBoxPlot <- function (
+  experimentList,
+  classifier,
+  data.type,
+  fig.height = 10,
+  fig.width = 4 + 0.2*length(experimentList),
+  experimentLabels = names(experimentList),
+  main = paste(sep = "", parameters$recountID, "; ", classifier,
+               "\n", data.type,
+               #"; ", variablesType,
+               "; ", parameters$iterations, "iterations"),
+  boxplotFile = NULL,
+  expColor = "#00BBFF",
+  permColor = "grey",
+  expLegend = "Train/test",
+  permLegend = "Permuted labels",
+  ....) {
 
 
-  # ## Define file to store the boxplot
-#  output.dir <- figure.dirs[classifier]
   testing.error.rates <- data.frame()
-  experimentNames <- names(experimentList)
 
+  ## Get experiment names to access the elements of experiment list
+  experiment.names <- names(experimentList)
+
+  ## Check that experiment labels are consistent with experiemnt list
+  if (length(experiment.names) != length(experimentLabels)) {
+    stop("length of experimentLabels (", length(experimentLabels),
+         ") is inconsistent with length of experimentList (", length(experimentList),").")
+  }
 
   ## Make sure the output directory exists for storing the plots
+  if (is.null(experiment.names) ||
+      length(names(experimentList)) < 1) {
+    stop("Not a single experiment for all variable composition")
+  }
 
-  if (is.null(experimentNames) || length(experimentNames) < 1) {
-    message.with.time("Not a single experiment for all variable composition")
-  } else {
-    exp <-1
-    for (exp in 1:length(experimentNames)) {
-      experiment <- experimentNames[exp]
-      exp.result <- experimentList[[experiment]]
-      if (exp == 1) {
-        testing.error.rates <- data.frame(exp.result$testing.error.rate)
-      } else {
-        testing.error.rates <-
-          cbind(testing.error.rates,
-                exp.result$testing.error.rate)
-      }
-    } # end iterate the all variables experiment
 
-    colnames(testing.error.rates) <- experimentNames
-    rownames(testing.error.rates) <- 1:nrow(testing.error.rates)
-
-    ## Define file to store the boxplot
-    file.prefix <- paste(sep="_",
-                         parameters$recountID,
-                         classifier,
-                         data.type,
-#                         variablesType,
-                         "iteration",
-                         parameters$iterations)
-    # outputParam <- outputParameters(experimentList,classifier = classifier, permute = FALSE,createDir = TRUE)
-    file.prefix <- file.prefix
-    if (permute) {
-      file.prefix <- paste(sep = "_", file.prefix, parameters$perm.prefix)
-    }# end if permuted class
-    boxplot.file <- file.path(parameters$dir$figures[classifier],
-                              paste(sep = "", file.prefix, "_boxplot.pdf"))
-    message.with.time("Boxplot file:\t", boxplot.file)
-    pdf(file = boxplot.file, width = 3 + 0.2*length(experimentNames), height = 12)
-
-    ## Define parameters for the boxplot
-    save.margins <- par("mar")
-    par(mar=c(14, 5, 5, 1))
-
-    ## Define colors for experiments with actual data and permutation tests
-    testTable.colors <- rep(x = expColor, length.out=ncol(testing.error.rates))
-    names(testTable.colors) <- colnames(testing.error.rates)
-    permTestExperiments <- grep(names(testTable.colors), pattern = "permLabels")
-    if (length(permTestExperiments > 1)) {
-      testTable.colors[permTestExperiments] <- permColor
+  exp <-1
+  for (exp in 1:length(experiment.names)) {
+    experimentName <- experiment.names[exp]
+    exp.result <- experimentList[[experimentName]]
+    if (exp == 1) {
+      testing.error.rates <- data.frame(exp.result$testing.error.rate)
+    } else {
+      testing.error.rates <-
+        cbind(testing.error.rates,
+              exp.result$testing.error.rate)
     }
+  } # end iterate the all variables experiment
+
+  # dim(testing.error.rates)
+  colnames(testing.error.rates) <- experimentLabels
+  rownames(testing.error.rates) <- 1:nrow(testing.error.rates)
+
+  if (!is.null(boxplotFile)) {
+    message.with.time("Boxplot file:\t", boxplotFile)
+    pdf(file = boxplotFile, width = fig.width, height = fig.height)
+  }
+
+  ## Define parameters for the boxplot
+  save.margins <- par("mar")
+  labelMargin <- (2 + max(nchar(experimentLabels)) * 0.45)
+  par(mar=c(labelMargin, 5, 5, 1))
+
+  ## Define colors for experiments with actual data and permutation tests
+  testTable.colors <- rep(x = expColor, length.out=ncol(testing.error.rates))
+  names(testTable.colors) <- colnames(testing.error.rates)
+  permTestExperiments <- grep(names(testTable.colors), pattern = "permLabels")
+  if (length(permTestExperiments > 1)) {
+    testTable.colors[permTestExperiments] <- permColor
+  }
 
 
-    ## Draw the box plot
-    boxplot(testing.error.rates,
-            horizontal = FALSE ,
-            ylab = "Misclassification rate", ylim=c(0,1),
-           # xlab = experimentNames,
-            main = main,
-            las=2 , cex.axis = 0.7,
-            col = testTable.colors
-    )
-    ## Draw horizontal grid
-    abline(h=seq(from = 0, to = 1, by = 0.1), col="darkgrey", lty="dotted")
-    abline(h=seq(from = 0, to = 1, by= 0.05 ), lty=2)
-    meanPermlabels <- apply(testing.error.rates[permTestExperiments], 1, mean)
+  ## Draw the box plot
+  boxplot(testing.error.rates,
+          horizontal = FALSE ,
+          ylab = "Misclassification rate", ylim=c(0,1),
+          # xlab = experimentLabels,
+          main = main,
+          las=2 , cex.axis = 1,
+          col = testTable.colors
+  )
+  ## Draw horizontal grid
+  abline(h=seq(from = 0, to = 1, by = 0.1), col="darkgrey", lty="dotted")
+  abline(h=seq(from = 0, to = 1, by= 0.05 ), lty=2)
+  meanPermlabels <- apply(testing.error.rates[permTestExperiments], 1, mean)
 
-    ## Mean misclassification rate for all the label-permuted tests
-    abline(h= mean(meanPermlabels), col="red", lwd=3 , lty= 3)
+  ## Mean misclassification rate for all the label-permuted tests
+  abline(h= mean(meanPermlabels), col="red", lwd=3 , lty= 3)
 
-    ## Draw a line with the theoretically computed random expected misclassification rate.
-    ## In principle, this should fit the mean of the permutation tests.
-    ## TO BE ONE BUT WE NEED TO EITHER PASS THE DATASET OR ASSOCIATE THIS METHOD TO THE CLASS.
-    # abline(h = , col="red", lwd=3 , lty= 3)
+  ## Draw a line with the theoretically computed random expected misclassification rate.
+  ## In principle, this should fit the mean of the permutation tests.
+  ## TO BE ONE BUT WE NEED TO EITHER PASS THE DATASET OR ASSOCIATE THIS METHOD TO THE CLASS.
+  # abline(h = , col="red", lwd=3 , lty= 3)
 
-    ## Plot legend
-    legend("topright", lwd = 4,
-           bty="o", bg = "white",
-           legend = c("Actual data", "Permuted Labels"),
-           col = c(expColor, permColor),
-           cex = 1, pch =0.2)
-    par(mar=save.margins)
+  ## Plot legend
+  legend("topleft", lwd = 4,
+         bty="o", bg = "white",
+         legend = c("Actual data", "Permuted Labels"),
+         col = c(expColor, permColor),
+         cex = 0.8, pch =0.2)
+  par(mar=save.margins)
+
+  if (!is.null(boxplotFile)){
     silence <- dev.off()
-  } # end else all variables experiments
+  }
 }
 
