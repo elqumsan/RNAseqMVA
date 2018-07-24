a701d5383b1eda0df7274a594d337df3d1dc65bf
elqumsan <m.qumsan@gmail.com>
2018-07-24 16:04:48 +0200

    stoping not required command line

--- misc/01b_load_parameters.R
+++ misc/01b_load_parameters.R
@@ -2,8 +2,8 @@ require("RNAseqMVA")
 
 source('R/required_bioconductor.R')
 source('R/required_cran.R')
-source('R/check_required_cran_packages.R')
-source('R/check_required_bioconductor_packages.R')
+# source('R/check_required_cran_packages.R')
+# source('R/check_required_bioconductor_packages.R')
 
 LoadRequiredCRANPackages(requiredCRAN)
 LoadRequiredBioconductorPackages(requiredBioconductor)
