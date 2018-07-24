b1d07fb056bd1b6aa28b19f58312f2c14d73b333
jvanheld <Jacques.van-Helden@univ-amu.fr>
2018-07-24 12:36:49 +0200

    split the two functions in two separated files

---
+++ R/required_cran_packages.R
@@ -0,0 +1,39 @@
+
+###################################################################
+################### Required Library #############################
+##################################################################
+
+#' @title Load required packages from CRAN, after having installed them if necessary.
+#' @author Mustafa AbuElQumsan and Jacques van Helden
+#' @description Load a list of required RCRAN packages. For each specified package,
+#' first check  if it is available on this R instance. If not, automatically install it.
+#' @param packages a vector containing a list of required CRAN packages.
+#' @return chack and then install all CRAN packages if it aren't installed.
+#' @export
+RequiredCRANPackages <- function(packages) {
+  for (pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      install.packages(pkg, dependencies = TRUE)
+    }
+    library(pkg, character.only = TRUE)
+  }
+}
+
+#' @title Load required packages from BioConductor, afte having installed them if necessary.
+#' @author Mustafa AbuElQumsan and Jacques van Helden
+#' @description Load a list of required BioConductor packages. For each specified package,
+#' first check  if it is available on this R instance. If not, automatically install it.
+#' @param packages a vector containing a list of required BioConductor packages.
+#' @return chack and then install all BioConductor packages if it aren't installed.
+#' @export
+RequiredBioconductorPackages <- function(packages) {
+  for (pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      source('http://bioconductor.org/biocLite.R')
+      # biocLite("BiocUpgrade") # note: I had to add this
+      biocLite(pkg)
+    }
+    library(pkg, character.only = TRUE)
+  }
+}
+
