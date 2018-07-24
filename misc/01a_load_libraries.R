bba23b24ef1701ef0900b2140ffed395f1a6cb00
jvanheld <Jacques.van-Helden@univ-amu.fr>
2018-07-24 12:49:43 +0200

    separated library loading from parameter loading

---
+++ misc/01a_load_libraries.R
@@ -0,0 +1,11 @@
+source('R/required_bioconductor.R')
+source('R/required_cran.R')
+source('R/load_required_cran_packages.R')
+source('R/load_required_bioconductor_packages.R')
+
+LoadRequiredCRANPackages(requiredCRAN)
+LoadRequiredBioconductorPackages(requiredBioconductor)
+
+## END OF SCRIPT
+#################################################################
+
