c535a170834c36abadc4306c0ea7bb7aed5267a8
jvanheld <Jacques.van-Helden@univ-amu.fr>
2018-07-24 12:48:17 +0200

    renamed CheckRequired...Packages tp LoadRequired...Packages

---
+++ R/load_required_cran_packages.R
@@ -0,0 +1,22 @@
+
+###################################################################
+################### Required Library #############################
+##################################################################
+
+#' @title Load required packages from CRAN, after having installed them if necessary.
+#' @author Mustafa AbuElQumsan and Jacques van Helden
+#' @description Load a list of required RCRAN packages. For each specified package,
+#' first check  if it is available on this R instance. If not, automatically install it.
+#' @param packages a vector containing a list of required CRAN packages.
+#' @return chack and then install all CRAN packages if it aren't installed.
+#' @export
+LoadRequiredCRANPackages <- function(packages) {
+  for (pkg in packages) {
+    if (!require(pkg, character.only = TRUE)) {
+      install.packages(pkg, dependencies = TRUE)
+    }
+    library(pkg, character.only = TRUE)
+  }
+}
+
+
