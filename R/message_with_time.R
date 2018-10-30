9630bb6616c959beaef4a4c4faeaaf653e1e094f
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-10-30 19:15:12 +0100

    revised the description

--- R/message_with_time.R
+++ R/message_with_time.R
@@ -1,10 +1,12 @@
-#' @title print a message preceded by currrent tume
+#' @title print a message preceded by currrent time
 #' @author Jacques van Helden and Mustafa AbuElQumsan
-#' @description such function in order to be able to compute the time has been exploit through the computation process.
+#' @description Print a message prepended with the current time in reverse order (YYYY-MM-DD) in order to match alphabetical order with chronological order.
 #' @param ... all parameters are passed to the message() function
+#'
 #' @return report message with time for each proccess.
+#'
 #' @export
-message.with.time <- function(...) {
+message.with.time <- function (...) {
   message(format(Sys.time(), "%Y-%m-%d_%H%M%S"), "\t", ...)
 }
 
