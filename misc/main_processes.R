338748b4668098f0b30396811d7d2c7f0f293b23
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2018-12-04 06:39:58 +0100

    changed the order of execution of the different analyses to start with the less time-consuming ones

--- misc/main_processes.R
+++ misc/main_processes.R
@@ -8,17 +8,21 @@ source('misc/01b_load_parameters.R')
 message.with.time("Loading and normalising raw count data")
 source('misc/02_load_and_normalise_counts.R')
 
-message.with.time( "Impact of normalisation on classifier performances")
-source('misc/06_all_variables_vs_all_PCs.R')
+## Run this first because it is faster than with all variables
+message.with.time( "Feature selection by first PCs")
+source('misc/07_PCA_impact_of_PC_number.R')
 
+## Test the impact of k on KNN performances
 message.with.time( "Impact of k on KNN performances")
 source('misc/13_knn_impact_of_parameters.R')
 
+## Test the imapct of kernel on SVM performances
 message.with.time( "Impact of kernel on SVM performances")
 source('misc/14_svm_impact_of_parameters.R')
 
-message.with.time( "Feature selection by first PCs")
-source('misc/07_PCA_impact_of_PC_number.R')
+## Run analyses with all variables and default parameters
+message.with.time( "Impact of normalisation on classifier performances")
+source('misc/06_all_variables_vs_all_PCs.R')
 
 message("ALL ANALYSES PERFORMED")
 
