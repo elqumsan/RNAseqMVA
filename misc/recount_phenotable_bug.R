9e4d5e709421e01e5baa1e967b315b7767030bf7
jvanheld <Jacques.van-Helden@univ-amu.fr>
2019-10-19 10:30:15 +0200

    added code download recount memory image files if not there + finished the test. The test now runs correctly with recount version 1.10.13.

--- misc/recount_phenotable_bug.R
+++ misc/recount_phenotable_bug.R
@@ -1,35 +1,55 @@
-#### Genes as features: the phenotable extraction works fine ####
+## This scripts aims at testing the loading of phenoTable with the recount package,  for genes and transcripts respectively
 
-## Download recount data if not already there
-data.dir <- "~/RNAseqMVA_workspace/data/SRP056295/"
-dir.create(data.dir, recursive = TRUE, showWarnings = FALSE)
-setwd(data.dir)
-if (!file.exists("rse_gene.Rdata")) {
+#### Load recount library and check version ####
+library(recount)
+# message(version$version.string)
+message("Recount version: ", packageDescription("recount")$Version)
 
+#### Re-install recount package if old version ####
+if (packageDescription("recount")$Version < "1.10.13") {
+  if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+  BiocManager::install("recount")
 }
 
-## Load a recount memory image
+#### Parameters ####
+recountID <- "SRP056295" ## an example of problematic dataset
+data.dir <- file.path("~/RNAseqMVA_workspace/data/", recountID)
+
+#### Download recount data if not already there ####
+dir.create(data.dir, recursive = TRUE, showWarnings = FALSE)
+setwd(data.dir)
+list.files()
+if (!file.exists("rse_gene.Rdata")) {
+  gene_url <- download_study(project = recountID, type = "rse-gene", download = TRUE)
+  message("\tdownloaded gene Rdata from ", gene_url)
+}
+if (!file.exists("rse_tx.RData")) {
+  tx_url <- download_study(project = recountID, type = "rse-tx", download = TRUE)
+  message("\tdownloaded transcript Rdata from ", tx_url)
+}
+
+#### Genes as features: the phenotable extraction works fine ####
+message("Loading gene data")
 load("rse_gene.Rdata")
 gene_counts <- scale_counts(rse_gene, by  = "mapped_reads")
 gene_pheno <- colData(gene_counts)
 gene_geochar <-   recount::geo_characteristics(gene_pheno)
 names(gene_geochar)
-head(gene_geochar)
-table(gene_geochar$tissue)
+print(head(gene_geochar))
+print(table(gene_geochar$tissue))
 
 #### Transcripts (tx) as features: the phenotable extraction fails ####
 
 ## Load a recount memory image
+message("Loading transcript data")
 load("rse_tx.RData")
 tx_counts <- scale_counts(rse_tx, by  = "mapped_reads")
 tx_pheno <- colData(tx_counts)
 tx_geochar <-   recount::geo_characteristics(tx_pheno)
 
 names(gene_geochar)
-## Problem: there is a single column named "c..tissue" instead of 2 columns named "tissue" and "cell type"
-
-head(tx_geochar)
-## This apparently comes from a parsing problem: the column name (cell type)
-## comes in the column content rather than as a column header
+print(head(tx_geochar))
+print(table(tx_geochar$tissue))
 
 
