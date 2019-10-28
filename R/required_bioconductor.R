cc8c310c21747a74c5184554d956796c38b3eaf5
jvanheld <Jacques.van-Helden@univ-amu.fr>
2019-10-29 00:36:32 +0100

    report problem with derfinder

--- R/required_bioconductor.R
+++ R/required_bioconductor.R
@@ -6,14 +6,20 @@
 ## These cannot be declared with @import, since they are cannot be installed via install.packages()
 requiredBioconductor <- c(
   "IRanges", # Seems to be required for derfinder but not automatically installed
-  "derfinder",
   "recount",
   "vsn", ## For RNA-seq plots from Huber
   "DESeq2",
   "limma",
   "edgeR",
   "S4Vectors",
+  "derfinder",
   "SummarizedExperiment")
 # LoadRequiredBioconductorPackages(requiredBioconductor)
 
 message("Required Bioconductor libraries:\n\t", paste(collapse="\n\t", requiredBioconductor))
+
+
+## PROBLEM WITH derfinder
+##  namespace ‘rlang’ 0.3.4 is already loaded, but >= 0.4.0 is required
+## Error: package or namespace load failed for ‘derfinder’ in loadNamespace(i, c(lib.loc, .libPaths()), versionCheck = vI[[i]]):
+## namespace ‘rlang’ 0.3.4 is already loaded, but >= 0.4.0 is required
