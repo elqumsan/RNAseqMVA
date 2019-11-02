46745f9f15d2ca40ce97c2bf255f30e1b6f85e6c
jvanheld <Jacques.van-Helden@univ-amu.fr>
2019-11-02 12:23:31 +0100

    working but sending jobs sequentially; will be replaced by a jobarray in another script

--- srun_analysis.sh
+++ srun_analysis.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-module load conda
-conda init bash
-conda activate rnaseqmva
+
+## Note: the loading of conda module and environment must apparently be done
+## in the sbatch that calls this script, and not within the script itself
+# module load conda
+# conda init bash
+# conda activate rnaseqmva
 cd /shared/projects/rnaseqmva/RNAseqMVA
-LOG_DIR=/shared/projects/rnaseqmva/logs
+WORKSPACE=/shared/projects/rnaseqmva/RNAseqMVA_workspace
+LOG_DIR=${WORKSPACE}/logs
 mkdir -p ${LOG_DIR}
 
 cd /shared/projects/rnaseqmva/RNAseqMVA
-
 START_DATE=`date +%Y-%m-%d_%H%M%S`
-OUT_FILE=${LOG_DIR}/log_${START_DATE}_out.txt
-ERR_FILE=${LOG_DIR}/log_${START_DATE}_err.txt
+
+
+## !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+## Second analysis
+
+## Parameters
+RECOUNT_ID=SRP066834
+FEATURE=gene
+PREFIX=${RECOUNT_ID}_${FEATURE}_${START_DATE}
 
 ## Submit the job to slurm job scheduler via srun
 srun --mem=32GB \
-  --output ${OUT_FILE} \
-  --error ${ERR_FILE} \
-  Rscript --vanilla misc/main_processes.R
+  --output ${LOG_DIR}/${PREFIX}_out.txt \
+  --error ${LOG_DIR}/${PREFIX}_err.txt \
+  Rscript --vanilla misc/main_processes.R ${RECOUNT_ID} ${FEATURE}
+
