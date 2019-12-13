3256c34c79ce560fc17e52aaaa9b56157abb82b4
jvanheld <Jacques.van-Helden@univ-amu.fr>
2019-12-13 18:35:00 +0100

    JvH and Mustafa revised the output directories

--- R/init_recount_id.R
+++ R/init_recount_id.R
@@ -20,10 +20,18 @@ initRecountID <- function(recountID, project.parameters) {
   ## Specify the current recountID in parameters
   parameters$recountID <- recountID
 
+  ## Get the feature type (gene or transcript)
+  if (is.null(parameters$feature)) {
+    parameters$feature <- "gene"
+    message("Beware: YAML configuration file did not specify the feature type -> set to gene")
+  }
+  message("\tFeature type\t", parameters$feature)
+  featureType <- parameters$feature
+
   ## studyPath, i.e. path where the data downloaded from Recount2 is stored
   parameters$studyPath <- file.path(
     parameters$dir$workspace, "data",
-    recountID)
+    paste0(recountID, "_", featureType))
 
 
   ## Overwrite default parameters wih project-specific parameters
@@ -41,17 +49,13 @@ initRecountID <- function(recountID, project.parameters) {
 
   #### Check some required parameters ####
 
-  ## Feature type must be specified
-  if (is.null(parameters$feature)) {
-    parameters$feature <- "gene"
-  }
-  message("\tFeature type\t", parameters$feature)
 
   ## Classifiers must be specified
   if (is.null(parameters$classifiers)) {
     stop("The configuration file must contain a parameter named 'classifiers'.")
   }
 
+  ## The list of classifiers cannot be empty
   if (length(parameters$classifiers) < 1) {
     stop("The configuration file must specify at least one classifier.")
   }
@@ -65,7 +69,7 @@ initRecountID <- function(recountID, project.parameters) {
 
   ##### Output directories ####
   if (is.null(parameters$dir)) {
-    parameters$dir <- list()
+    stop("Output directories (main, workspace) must be specified in the config file. ")
   }
 
   ## Check required directories
@@ -80,16 +84,18 @@ initRecountID <- function(recountID, project.parameters) {
 
   ## Result directory
   if (is.null(parameters$dir$results)) {
-    parameters$dir$results <- file.path(parameters$dir$workspace, "results")
+    parameters$dir$results <- file.path(
+      parameters$dir$workspace,
+      "results",
+      paste0(recountID, "_", parameters$feature))
   }
   dir.create(parameters$dir$results, showWarnings = FALSE, recursive = TRUE)
   message("\t\tresults\t", parameters$dir$results)
 
-  ## Sub-directory to save the general figures of this study case
+  ## Sub-directory to save the generic figures of this study case (for example, comparisons between study cases)
   if (is.null(parameters$dir$figures)) {
     parameters$dir$figures <- file.path(
       parameters$dir$results,
-      paste(sep = "_", recountID, parameters$feature),
       "figures")
   }
   dir.create(path = parameters$dir$figures, recursive = TRUE, showWarnings = FALSE)
@@ -99,7 +105,6 @@ initRecountID <- function(recountID, project.parameters) {
   if (is.null(parameters$dir$tsv)) {
     parameters$dir$tsv <- file.path(
       parameters$dir$results,
-      paste(sep = "_", recountID, parameters$feature),
       "TSV")
   }
   dir.create(path = parameters$dir$tsv, recursive = TRUE, showWarnings = FALSE)
@@ -111,25 +116,24 @@ initRecountID <- function(recountID, project.parameters) {
   if (is.null(parameters$dir$classifiers)) {
     parameters$dir$classifiers <- file.path(
       parameters$dir$results,
-      paste(sep = "_", recountID, parameters$feature),
       parameters$classifiers)
   }
   names(parameters$dir$classifiers) <- parameters$classifiers
-  message("\t\tclassifiers\t", parameters$dir$classifiers)
+  message("\tclassifiers\n\t\t", paste(collapse="\n\t\t", parameters$dir$classifiers))
 
   if (is.null(parameters$dir$classifier_tables)) {
     parameters$dir$classifier_tables <- file.path(
       parameters$dir$classifiers, "tables")
   }
   names(parameters$dir$classifier_tables) <- parameters$classifiers
-  message("\t\tclassifier_tables\t", parameters$dir$classifier_tables)
+  message("\tclassifier_tables\n\t\t", paste(collapse = "\n\t\t", parameters$dir$classifier_tables))
 
   if (is.null(parameters$dir$classifier_figures)) {
     parameters$dir$classifier_figures <- file.path(
       parameters$dir$classifiers, "figures")
   }
   names(parameters$dir$classifier_figures) <- parameters$classifiers
-  message("\t\tclassifier_figures\t", parameters$dir$classifier_figures)
+  message("\tclassifier_figures\n\t\t", paste(collapse = "\n\t\t", parameters$dir$classifier_figures))
 
   ## Create all the recountID-specific sub-directories
   for (dir in c(parameters$dir$classifiers,
@@ -145,7 +149,6 @@ initRecountID <- function(recountID, project.parameters) {
   if (is.null(parameters$dir$NormalizationImpact)) {
     parameters$dir$NormalizationImpact  <- file.path(
       parameters$dir$results,
-      paste(sep = "_", recountID, parameters$feature),
       paste("impact_of_normalisation", sep = ""))
   }
   dir.create(parameters$dir$NormalizationImpact, showWarnings = F, recursive = T)
@@ -155,7 +158,6 @@ initRecountID <- function(recountID, project.parameters) {
   if (is.null(parameters$dir$PCviz)) {
     parameters$dir$PCviz <- file.path(
       parameters$dir$results,
-      paste(sep = "_", recountID, parameters$feature),
       paste( "visualization_of_PCs", sep = ""))
   }
   dir.create(parameters$dir$PCviz, showWarnings = F, recursive = T)
