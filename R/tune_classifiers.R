4b77ace1e4892a07f39291677e237d023484b14d
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2020-08-07 15:54:24 +0200

    implemented a first version of TuneClassifiers()

--- R/tune_classifiers.R
+++ R/tune_classifiers.R
@@ -3,23 +3,40 @@
 #' @param studyCase a studyCase object
 #' @param dataType="TMM_log2" data type to use for the tuning. Must be one of the data types included in the 'datasetsForTest' attribute of the studyCase object
 #' @param tuneSVM=TRUE if TRUE, tune parameters for SVM
-#' @param tuneRF=TRUE if TRUE, tune parameters for Random Forest
+#' @param svmCost=1 cost parameter values for SVM
+#' @param svmDegree=1:4 degree values for SVM polynomial kernel
+#' @param svmGamma=4^(-2:4) gamma values for SVM kernels (ignored for linear kernel)
+#' @param tuneRandomForest=TRUE if TRUE, tune parameters for Random Forest
+#' @param rfNtree=c(100,200,300,500) values to test for RF numbers of trees
+#' @param rfMtry=c(50,100,200,300,500) values to test for RF mtry parameter
 #' @param tuneKNN=TRUE if TRUE, tune parameters for KNN
-#' @param plotResults=TRUE if TRUE, run plot()  on the tuned objects
+#' @param knnK=c(1,2,3,4,5,7,10) number of neighbours for KNN
+#' @param knnL=0 minimum vote for definite decision for KNN
+#' @param plotResults=FALSE if TRUE, run plot()  on the tuned objects
 #' @import e1071
 #' @import randomForest
 #' @return a list of parameters cloned from the studyCase, added with the optimal parameters identified here
 #' @export
 TuneClassifiers <- function(studyCase,
-                            dataType="TMM_log2",
+                            dataType="TMM_log2_PC",
                             tuneSVM=TRUE,
+#                            svmCost=4^(-2:2),
+                            svmCost=1,
+                            svmDegree=1:4,
+                            svmGamma=4^(-2:4),
+                            svmCoef0=4^(-2:4),
                             tuneRF=TRUE,
-                            tuneKNN=TRUE) {
+                            rfNtree=c(100,200,300,500),
+                            rfMtry=c(50,100,200,300,500),
+                            tuneKNN=TRUE,
+                            knnK=c(1,2,3,4,5,7,10),
+                            knnL=0,
+                            plotResults=FALSE) {
 
   ## Prepare the result object
   result <- list()
   result$parameters <- studyCase$parameters
-  result$tuneResults <-
+  result$tuneResults <- list()
 
 
   #### Data selection ####
@@ -34,59 +51,131 @@ TuneClassifiers <- function(studyCase,
 
   #### Tuning of the SVM parameters ####
   if (tuneSVM) {
+    message("\tTuneClassifiers()\tTuning SVM")
+    result$tuneResults$svm <- list()
 
     #### Parameters for the linear kernel ####
+    message.with.time("\t\tTuning SVM with linear kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     linearTuning <- tune.svm(x = t(dataSet$dataTable),
                              y = as.factor(dataSet$classLabels),
-                             #                 cost = 4^(-2:2),
+                             cost = svmCost,
                              kernel = "linear")
-    linearTuning
-    plot(linearTuning, main = "SVM tuning - linear kernel")
-    alarm()
+    result$tuneResults$svm$linearTuning <- linearTuning
+    if (plotResults) {
+      plot(linearTuning, main = "SVM tuning - linear kernel")
+    }
 
 
     #### Parameters for the polynomial kernel ####
+    message.with.time("\t\tTuning SVM with polynomial kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     polynomialTuning <- tune.svm(x = t(dataSet$dataTable),
                                  y = as.factor(dataSet$classLabels),
-                                 #                 cost = 4^(-2:2),
-                                 degree = 1:4,
+                                 cost = svmCost,
+                                 gamma = svmGamma,
+                                 degree = svmDegree,
+                                 coef0 = svmCoef0,
                                  kernel = "polynomial")
-    polynomialTuning
-    plot(polynomialTuning, main = "SVM tuning - polynomial kernel")
-    alarm()
+    result$tuneResults$svm$polynomialTuning <- polynomialTuning
+    if (plotResults) {
+      plot(polynomialTuning, main = "SVM tuning - polynomial kernel")
+    }
+
 
     #### Parameters for the radial kernel ####
+    message.with.time("\t\tTuning SVM with radial kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     radialTuning <- tune.svm(x = t(dataSet$dataTable),
                              y = as.factor(dataSet$classLabels),
-                             #                 cost = 4^(-2:2),
-                             gamma = 4^(-2:4),
-                             coef0 = 4^(-2:4),
+                             cost = svmCost,
+                             gamma = svmGamma,
                              kernel = "radial")
-    radialTuning
-    plot(radialTuning, main = "SVM tuning - radial kernel")
-    alarm()
+    result$tuneResults$svm$radialTuning <- radialTuning
+    if (plotResults) {
+      plot(radialTuning, main = "SVM tuning - radial kernel")
+    }
 
-    # obj3 <- best.svm(x = t(dataSet$dataTable),
-    #                  y = as.factor(dataSet$classLabels), kernel = "linear")
-    # obj3
-    # plot(obj3)
+    #### Parameters for the sigmoid kernel ####
+    message.with.time("\t\tTuning SVM with sigmoid kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
+    sigmoidTuning <- tune.svm(x = t(dataSet$dataTable),
+                              y = as.factor(dataSet$classLabels),
+                              cost = svmCost,
+                              gamma = svmGamma,
+                              coef0 = svmCoef0,
+                              kernel = "sigmoid")
+    result$tuneResults$svm$sigmoidTuning <- sigmoidTuning
+    if (plotResults) {
+      plot(sigmoidTuning, main = "SVM tuning - sigmoid kernel")
+    }
+
   }
 
-    #### Tune Random Forest parameters ####
+  #### Tune Random Forest parameters ####
+  if (tuneRandomForest) {
+    message("\tTuneClassifiers()\tTuning RF")
+    result$tuneResults$rf <- list()
 
-  tunedRF <- randomForest::tuneRF(
-    x = t(dataSet$dataTable),
-    y = as.factor(dataSet$classLabels),
-    #                     mtryStart = 8,
-    ntreeTry = 500,
-    stepFactor = 1.5,
-    improve = 0.025,
-    trace = TRUE,
-    plot = TRUE,
-    doBest = FALSE)
-  plot(tunedRF)
-  alarm()
+    ## Tune random forest with e1071::tune.randomForest() function
+    tunedRandomForest  <-
+      tune.randomForest(x = t(dataSet$dataTable),
+                        y = as.factor(dataSet$classLabels),
+                        nodesize = 1,
+                        mtry = rfMtry,
+                        ntree = rfNtree)
+    result$tuneResults$rf$tunedRandomForest <- tunedRandomForest
+    if (plotResults) {
+      plot(tunedRandomForest, main = "RF tuning")
+    }
 
+
+    # ## use randomForest::tuneRF to tune RF
+    # tunedRF <- randomForest::tuneRF(
+    #   x = t(dataSet$dataTable),
+    #   y = as.factor(dataSet$classLabels),
+    #   #                     mtryStart = 8,
+    #   ntreeTry = 500,
+    #   stepFactor = 1.5,
+    #   improve = 0.025,
+    #   trace = TRUE,
+    #   plot = TRUE,
+    #   doBest = FALSE)
+    # result$tuneResults$rf$tunedRF <- tunedRF
+    # if (plotResults) {
+    #   plot(tunedRF, main = "RF tuning")
+    # }
+
+  }
+
+  #### Tune KNN parameters ####
+  if (tuneKNN) {
+    message("\tTuneClassifiers()\tTuning KNN")
+    result$tuneResults$knn <- list()
+
+    tunedKNN <- tune.knn(x = t(dataSet$dataTable),
+                         y = as.factor(dataSet$classLabels),
+                         k = knnK,
+                         l = knnL)
+
+    result$tuneResults$knn$tunedKNN <- tunedKNN
+    if (plotResults) {
+#      plot(tunedKNN, main = "KNN tuning")
+      plot(tunedKNN$performances$k,
+           tunedKNN$performances$error,
+           type = "b",
+           main = paste(sep = "",
+                        studyCase$ID,
+                        " " , studyCase$parameters$feature,
+                        " - ", studyCase$parameters$short_label,
+                        "\n", "KNN tuning"),
+           xlab = "k",
+           ylab = "MER",
+           las = 1,
+           panel.first = grid(),
+           col = "red"
+           )
+    }
+
+  }
+
+  return(result)
 
 }
 
