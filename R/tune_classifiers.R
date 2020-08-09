27d88f2d2959d8c10161d652583146dee6042d3c
jvanheld <Jacques.van-Helden@univ-amu.fr>
2020-08-09 16:06:13 +0200

    progressing on the plotting functions after classifier tuning

--- R/tune_classifiers.R
+++ R/tune_classifiers.R
@@ -35,10 +35,11 @@ TuneClassifiers <- function(studyCase,
   ## Prepare the result object
   result <- list()
   result$parameters <- studyCase$parameters
+  result$dataType <- dataType
   result$tuneResults <- list()
 
 
-  #### Data selection ####
+  ## Data selection
   dataSet <- studyCase$datasetsForTest[[dataType]]
   if (is.null(dataSet)) {
     stop("dataType not supported for this studyCase: ", dataType,
@@ -48,12 +49,12 @@ TuneClassifiers <- function(studyCase,
 
 
 
-  #### Tuning of the SVM parameters ####
+  ## Tuning of the SVM parameters
   if (tuneSVM) {
     message.with.time("\tTuneClassifiers()\tTuning SVM")
     result$tuneResults$svm <- list()
 
-    #### Parameters for the linear kernel ####
+    ## Parameters for the linear kernel
     message.with.time("\t\tTuning SVM with linear kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     linearTuning <- tune.svm(x = t(dataSet$dataTable),
                              y = as.factor(dataSet$classLabels),
@@ -65,7 +66,7 @@ TuneClassifiers <- function(studyCase,
     }
 
 
-    #### Parameters for the polynomial kernel ####
+    ## Parameters for the polynomial kernel
     message.with.time("\t\tTuning SVM with polynomial kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     polynomialTuning <- tune.svm(x = t(dataSet$dataTable),
                                  y = as.factor(dataSet$classLabels),
@@ -80,7 +81,7 @@ TuneClassifiers <- function(studyCase,
     }
 
 
-    #### Parameters for the radial kernel ####
+    ## Parameters for the radial kernel
     message.with.time("\t\tTuning SVM with radial kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     radialTuning <- tune.svm(x = t(dataSet$dataTable),
                              y = as.factor(dataSet$classLabels),
@@ -92,7 +93,7 @@ TuneClassifiers <- function(studyCase,
       #      plot(radialTuning, main = "SVM tuning - radial kernel")
     }
 
-    #### Parameters for the sigmoid kernel ####
+    ## Parameters for the sigmoid kernel
     message.with.time("\t\tTuning SVM with sigmoid kernel\t", studyCase$ID, " ", studyCase$parameters$feature)
     sigmoidTuning <- tune.svm(x = t(dataSet$dataTable),
                               y = as.factor(dataSet$classLabels),
@@ -107,7 +108,7 @@ TuneClassifiers <- function(studyCase,
 
   }
 
-  #### Tune Random Forest parameters ####
+  ## Tune Random Forest parameters
   if (tuneRandomForest) {
     message.with.time("\tTuneClassifiers()\tTuning RF")
     result$tuneResults$rf <- list()
@@ -122,7 +123,7 @@ TuneClassifiers <- function(studyCase,
     result$tuneResults$rf$tunedRandomForest <- tunedRandomForest
 
     if (plotResults) {
-      plotRFtuning(tunedRandomForest)
+      plotRFtuning(result)
     }
 
 
@@ -144,7 +145,7 @@ TuneClassifiers <- function(studyCase,
 
   }
 
-  #### Tune KNN parameters ####
+  ## Tune KNN parameters
   if (tuneKNN) {
     message.with.time("\tTuneClassifiers()\tTuning KNN")
     result$tuneResults$knn <- list()
@@ -157,7 +158,7 @@ TuneClassifiers <- function(studyCase,
     result$tuneResults$knn$tunedKNN <- tunedKNN
     if (plotResults) {
       #      plot(tunedKNN, main = "KNN tuning")
-      plotKNNtuning(tunedKNN)
+      plotKNNtuning(result)
     }
 
   }
@@ -169,29 +170,28 @@ TuneClassifiers <- function(studyCase,
 
 #' @title draw plots to highlight the results of ranfomForest tuning
 #' @author Jacques van Helden
-#' @description draw a series of plots highlighting the impact of the parameters (mtry, ntrees) on the performances (miclassification error rate) of randomForest
-#' @param tunedRandomForest result of e1071::tune.randomForest() where vectors of values were provided for mtry and ntrees
+#' @description draw a series of plots highlighting the impact of the parameters (mtry, ntree) on the performances (miclassification error rate) of randomForest
+#' @param tuningResult result of TuneClassifiers() run with option tuneRandomForest=TRUE, and where vectors of values were provided for mtry and ntree
+#' @param pdfFile=NULL if not null, the plot is exported in pdf format the specified file. Else it is printed out on the current device.
+#' @param ... additional parameters are passed to plot()
 #' @return no return field
 #' @export
-plotRFtuning <- function(tunedRandomForest) {
+plotRFtuning <- function(tuningResult, pdfFile = NULL, ...) {
+
+  ## Open pdf file if specified
+  if (!is.null(pdfFile)) {
+    message("\tplotKNNtuning() exporting to pdf file\t", pdfFile)
+    pdf(pdfFile, width = 7, height = 5)
+  }
+
   par(mfrow = c(2,2))
-  #      plot(tunedRandomForest, main = "RF tuning")
-  # RSAGA::grid.to.xyz(data = tunedRandomForest$performances,
-  #                    varname = "error",
-  #                    colnames = c("mtry", "ntree", "error"))
-  errorMatrix <- tidyr::spread(data = tunedRandomForest$performances[, c("mtry", "ntree", "error")],
-                               key = "ntree",
-                               value = "error")
-  persp(x = errorMatrix$mtry,
-        y = as.numeric(colnames(errorMatrix[, -1])),
-        z = as.matrix(errorMatrix[, -1]),
-        theta = 235,
-        main = paste(sep = "",
-                     studyCase$ID,
-                     " " , studyCase$parameters$feature,
-                     " - ", studyCase$parameters$short_label,
-                     "\n", "RF tuning"),
-        xlab = "mtry", ylab = "ntree", zlab = "error")
+
+  ## Get the randomForest tuning result
+  tunedRandomForest <- tuningResult$tuneResults$rf$tunedRandomForest
+  if (is.null(tunedRandomForest)) {
+    stop("plotRFtuning() error: tuningResult does not contain any random forest tuning results")
+  }
+
 
   ## Get best parameters
   bestNtree <- tunedRandomForest$best.parameters$ntree
@@ -201,14 +201,17 @@ plotRFtuning <- function(tunedRandomForest) {
   ## Plot impact of ntree for the best values of the other parameters
   ntreeImpact <- subset(x = tunedRandomForest$performances,
                         mtry == bestMtry & nodesize == bestNodeSize)
+
   plot(ntreeImpact$ntree, ntreeImpact$error, type = "b",
-       xlab = "ntree", ylab = "MER",
+       xlab = "ntree", ylab = "MER", cex.axis = 0.7,
        panel.first = grid(), col = "red", las = 1,
        main = paste(sep = "",
-                    studyCase$ID,
-                    " " , studyCase$parameters$feature,
-                    " - ", studyCase$parameters$short_label,
+                    tuningResult$parameters$recountID,
+                    " " , tuningResult$parameters$feature,
+                    " - " , tuningResult$dataType,
+                    "\n", tuningResult$parameters$short_label,
                     "\n", "RF ntree tuning"),
+       ...
   )
 
   ## Plot impact of mtry for the best values of the other parameters
@@ -218,35 +221,83 @@ plotRFtuning <- function(tunedRandomForest) {
        xlab = "mtry", ylab = "MER",
        panel.first = grid(), col = "red", las = 1,
        main = paste(sep = "",
-                    studyCase$ID,
-                    " " , studyCase$parameters$feature,
-                    " - ", studyCase$parameters$short_label,
+                    tuningResult$parameters$recountID,
+                    " " , tuningResult$parameters$feature,
+                    " - " , tuningResult$dataType,
+                    "\n", tuningResult$parameters$short_label,
                     "\n", "RF mtry tuning"),
+
+       ...
   )
+
+  ## 3D plot of the simultaneous impact of ntree and mtry
+  errorMatrix <- tidyr::spread(data = tunedRandomForest$performances[, c("mtry", "ntree", "error")],
+                               key = "ntree",
+                               value = "error")
+  persp(x = errorMatrix$mtry,
+        y = as.numeric(colnames(errorMatrix[, -1])),
+        z = as.matrix(errorMatrix[, -1]),
+        #        zlim = c(0,1),
+        theta = 235,
+        main = paste(sep = "",
+                     studyCase$ID,
+                     " " , studyCase$parameters$feature,
+                     " - ", studyCase$parameters$short_label,
+                     "\n", "RF tuning"),
+        xlab = "mtry", ylab = "ntree", zlab = "error")
+
+
   par(mfrow = c(1,1))
+
+  ## Close pdf file if specified
+  if (!is.null(pdfFile)) {
+    silence <- dev.off()
+  }
 
 }
 
 #' @title draw plots to highlight the results of KNN tuning
 #' @author Jacques van Helden
 #' @description draw a series of plots highlighting the impact of the number of neighbours (k) on the performances (miclassification error rate) of KNN
-#' @param tunedKNN result of e1071::tune.knn() where a vector of several values was provided for k
+#' @param tuningResult result of TuneClassifiers() run with option tuneKNN=TRUE, and where a vector of several values was provided for k
+#' @param pdfFile=NULL if not null, the plot is exported in pdf format the specified file. Else it is printed out on the current device.
+#' @param ... additional parameters are passed to plot()
 #' @return no return field
 #' @export
-plotKNNtuning <- function(tunedKNN) {
+plotKNNtuning <- function(tuningResult, pdfFile = NULL, ...) {
+
+  ## Open pdf file if specified
+  if (!is.null(pdfFile)) {
+    message("\tplotKNNtuning() exporting to pdf file\t", pdfFile)
+    pdf(pdfFile, width = 7, height = 5)
+  }
+
+  ## Get the KNN tuning result
+  tunedKNN <- tuningResult$tuneResults$knn$tunedKNN
+  if (is.null(tunedKNN)) {
+    stop("plotKNNtuning() error: tuningResult does not contain any KNN tuning results")
+  }
+
+
   plot(tunedKNN$performances$k,
        tunedKNN$performances$error,
        type = "b",
        main = paste(sep = "",
-                    studyCase$ID,
-                    " " , studyCase$parameters$feature,
-                    " - ", studyCase$parameters$short_label,
+                    tuningResult$parameters$recountID,
+                    " " , tuningResult$parameters$feature,
+                    " - " , tuningResult$dataType,
+                    "\n", tuningResult$parameters$short_label,
                     "\n", "KNN tuning"),
        xlab = "k",
        ylab = "MER",
        las = 1,
        panel.first = grid(),
-       col = "red"
+       col = "red",
+       ...
   )
 
+  ## Close pdf file if specified
+  if (!is.null(pdfFile)) {
+    silence <- dev.off()
+  }
 }
