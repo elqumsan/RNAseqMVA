8d3c819a3fedf3cb5815bf36ddf5ca5462f6abd5
jvanheld <Jacques.van-Helden@univ-amu.fr>
2020-08-09 20:26:23 +0200

    suppressed non-ASCII letters

--- R/required_bioconductor.R
+++ R/required_bioconductor.R
@@ -1,7 +1,3 @@
-
-#require("roxygen2") ## MUSTAFA: do we really required roxygen2 and devtools to run the scripts ? They are required for building the package, but maybe not for running the analyses
-#require("devtools") ## MUSTAFA: do we really required roxygen2 and devtools to run the scripts ? They are required for building the package, but maybe not for running the analyses
-
 ## Bioconductor packages required for the analyses.
 ## These cannot be declared with @import, since they are cannot be installed via install.packages()
 requiredBioconductor <- c(
@@ -14,12 +10,6 @@ requiredBioconductor <- c(
   "S4Vectors",
   "derfinder",
   "SummarizedExperiment")
-# LoadRequiredBioconductorPackages(requiredBioconductor)
 
 message("Required Bioconductor libraries:\n\t", paste(collapse="\n\t", requiredBioconductor))
 
-
-## PROBLEM WITH derfinder
-##  namespace ‘rlang’ 0.3.4 is already loaded, but >= 0.4.0 is required
-## Error: package or namespace load failed for ‘derfinder’ in loadNamespace(i, c(lib.loc, .libPaths()), versionCheck = vI[[i]]):
-## namespace ‘rlang’ 0.3.4 is already loaded, but >= 0.4.0 is required
