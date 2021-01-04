6e9a62b2ac53ccd68a9d49a80931f1e52ac42c5f
jvanheld <Jacques.van-Helden@univ-amu.fr>
2021-01-04 07:02:19 +0100

    implemented the gathering of SVM kernel results for all study cases and feature types

--- misc/14_svm_compare_results.R
+++ misc/14_svm_compare_results.R
@@ -1,9 +1,11 @@
 #### Reload SVM results from the different study cases in order to compare performances ####
 
-## Initialise variable to store results
+#### Initialise variable to store results ####
 kernel.impact.results <- list()
+testingMERmean <- list()
+testingMERsd <- list()
 
-## Parameter values
+#### Parameter values ####
 featureTypes <- c("gene", "transcript")
 recountIDs <- c(
   "SRP042620",
@@ -15,10 +17,42 @@ recountIDs <- c(
   "SRP066834"
 )
 
-## Default values for testing
-featureType <- "transcript"
-recountID <- "SRP042620"
 
+## Kernels
+kernels <- c(
+  "radial",
+  "sigmoid",
+  "polynomial",
+  "linear"
+)
+
+## Normalisation methods
+normMethods <- c(
+  "filtered_counts",
+  "q0.75",
+  "TMM",
+  "RLE",
+  "q0.75_log2",
+  "TMM_log2",
+  "RLE_log2",
+  "q0.75_log2_PC",
+  "TMM_log2_PC",
+  "RLE_log2_PC"
+)
+
+## Permutation test suffixes
+permSuffixes <- c("", "_permLabels")
+
+#### Default values for debugging and testing ####
+featureType <- "gene"
+recountID <- "SRP042620"
+normMethod <- "TMM_log2"
+kernel <- "linear"
+permSuffix <- ""
+
+
+
+#### Reload the results ####
 for (featureType in featureTypes) {
   for (recountID in recountIDs) {
 
@@ -47,5 +81,56 @@ for (featureType in featureTypes) {
   message("\t", paste(collapse = ", ", names(kernel.impact.results[[featureType]])))
 }
 
+#### Compare performances between kernels for each study case ####
+for (featureType in featureTypes) {
+  for (recountID in recountIDs) {
+
+    ## Initialise the comparison tables
+    testingMERmean[[featureType]][[recountID]] <-
+      data.frame(matrix(nrow = length(normMethods),
+                        ncol = length(kernels) * length(permSuffixes)))
+    rownames(testingMERmean[[featureType]][[recountID]]) <- normMethods
+    colnames(testingMERmean[[featureType]][[recountID]]) <-
+      apply(expand.grid(kernels, permSuffixes), 1, paste, collapse="")
+
+    testingMERsd[[featureType]][[recountID]] <-
+      data.frame(matrix(nrow = length(normMethods),
+                        ncol = length(kernels) * length(permSuffixes)))
+    rownames(testingMERsd[[featureType]][[recountID]]) <- normMethods
+    colnames(testingMERsd[[featureType]][[recountID]]) <-
+      apply(expand.grid(kernels, permSuffixes), 1, paste, collapse="")
+
+    for (permSuffix in permSuffixes) {
+      for (normMethod in normMethods) {
+        for (kernel in kernels) {
+          resultName <- paste(sep="_",
+                              recountID,
+                              featureType,
+                              "svm",
+                              kernel,
+                              normMethod)
+          resultName <- paste0(resultName, permSuffix)
+          if (resultName %in% names(kernel.impact.results[[featureType]][[recountID]])) {
+            result <- kernel.impact.results[[featureType]][[recountID]][[resultName]]
+            # names(result)
+            # View(result)
+            ## Compute mean misclassification error rate
+            testingMERmean[[featureType]][[recountID]][normMethod, paste0(kernel, permSuffix)] <-
+              mean(result$testing.error.rate)
+            testingMERsd[[featureType]][[recountID]][normMethod, paste0(kernel, permSuffix)] <-
+              sd(result$testing.error.rate)
+          } else {
+            warning("Missing result\t", resultName)
+          }
+        }
+      }
+    }
+  }
+}
+
+testingMERmean[[featureType]][[recountID]]
+
+
+testingMERsd[[featureType]][[recountID]]
 
 
