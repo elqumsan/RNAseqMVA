199ae032b95ba6619ace56881065cdb8e7c68efe
VAN HELDEN Jacques <Jacques.van-Helden@univ-amu.fr>
2025-06-22 16:32:46 +0200

    reordered tasks in main_processes.R according to the file numbering

--- misc/main_processes.R
+++ misc/main_processes.R
@@ -47,33 +47,31 @@ if (studyCase$parameters$iterations < project.parameters$global$iterations) {
 message.with.time("Initializing parallel computing")
 source('misc/01c_init_parallel_computing.R')
 
-#### Test the impact of kernel on SVM performances ####
-message.with.time("Impact of kernel on SVM performances")
-source('misc/14_svm_impact_of_parameters.R')
 
-
-stop("User-requested stop at this level")
-
-#### Test the impact of k on KNN performances ####
-message.with.time("Impact of k on KNN performances")
-source('misc/13_knn_impact_of_k.R')
-
-#### Run analyses with all variables and default parameters ####
-message.with.time("Impact of normalisation on classifier performances")
-source('misc/06_all_variables_vs_all_PCs.R')
-
-
-
-#### Feature selection with first PCs ####
-message.with.time("Feature selection by first PCs")
-source('misc/07_PCA_impact_of_PC_number.R')
+## stop("User-requested stop at this level")
 
 
 #### Tune parameters ####
 message.with.time("Tuning parameters")
 source('misc/05_tune_parameters.R')
 
+#### Run analyses with all variables and default parameters ####
+message.with.time("Impact of normalisation on classifier performances")
+source('misc/06_all_variables_vs_all_PCs.R')
 
-message("YEAH! ALL ANALYSES HAVE BEEN PERFORMED")
+#### Feature selection with first PCs ####
+message.with.time("Feature selection by first PCs")
+source('misc/07_PCA_impact_of_PC_number.R')
+
+#### Test the impact of k on KNN performances ####
+message.with.time("Impact of k on KNN performances")
+source('misc/13_knn_impact_of_k.R')
+
+#### Test the impact of kernel on SVM performances ####
+message.with.time("Impact of kernel on SVM performances")
+source('misc/14_svm_impact_of_parameters.R')
+
+
+message.with.time("ALL ANALYSES HAVE BEEN PERFORMED")
 
 
