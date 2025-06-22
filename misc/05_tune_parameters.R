93d5bc02675c7aa22106d50d11fe542bbc1370c4
VAN HELDEN Jacques <Jacques.van-Helden@univ-amu.fr>
2025-06-22 17:24:17 +0200

    Used claude.ai to revise the syntax of the code documentaiton, and code indentation.
    No modification to the code itself.

--- misc/05_tune_parameters.R
+++ misc/05_tune_parameters.R
@@ -1,24 +1,39 @@
-#### Use e1071::tune() wrapper to find optimal parameters for the different classifiers ####
+# ==============================================================================
+# Parameter Tuning for Machine Learning Classifiers
+# ==============================================================================
+# This script uses e1071::tune() wrapper to find optimal parameters for
+# different classifiers (SVM, Random Forest, KNN).
+#
+# Note: e1071::tune() functions are very convenient for analyzing user-specified
+# combinations of parameters, but they run all tests sequentially, which may
+# take significant time for large datasets and high numbers of parameter values.
+#
+# Additionally, the tuning tests each parameter value only once, but we have
+# observed strong fluctuations between successive runs for some parameters
+# (e.g., ntrees and mtry parameters of randomForest). For this reason, we
+# developed a specific procedure to evaluate parameter impact with a
+# user-specified number of iterations to assess performance fluctuations
+# between independent runs.
+#
+# This code is kept for comparison purposes.
+# ==============================================================================
 
-## Note: e1071::tune() functions are versy convenient to analyse user-specified combinations of
-## parameters but they run all the tests sequentially, which may take a huge time for big datasets
-## (as the ones we analyse) and if the number of parameter values is high.
-##
-## Moreover, the tuning tests each parameter value only once, but we noticed that for some parameters
-## there are strong fluctuations between successive runs (e.g. the ntrees and mtry parameters of randomForest).
-## For this reason, we developed a specific procedure to run evaluate the impact of parameters with a
-## user-specified number of iterations (which enables us to evaluate the fluctuatins of performances
-## between independent runs).
-##
-## We however keep this code for the sake of comparison.
+# Configuration ----------------------------------------------------------------
 
-## Select a data type for the tuning
+# Select data type for tuning
 dataType <- "TMM_log2"
 
-
-## Define the path to the memory image for this test (compare classifier whenn they use all variables as features)
-dir.create(project.parameters$global$dir$memoryImages, showWarnings = FALSE, recursive = TRUE)
+# Define feature type
 featureType <- project.parameters$global$feature
+
+# Setup directories and file paths --------------------------------------------
+
+# Create memory images directory if it doesn't exist
+dir.create(project.parameters$global$dir$memoryImages,
+           showWarnings = FALSE,
+           recursive = TRUE)
+
+# Define path for saving results
 save.result.file <- file.path(
   project.parameters$global$dir$memoryImages,
   paste0(
@@ -26,41 +41,59 @@ save.result.file <- file.path(
     "_", featureType,
     "_", "tune-classifiers",
     "_", dataType,
-    "_results.Rdata"))
+    "_results.Rdata"
+  )
+)
+
+# Uncomment to load existing results
 # load(save.result.file)
 
-#### Run the analysis ####
+# Run parameter tuning analysis -----------------------------------------------
+
+message.with.time("Starting parameter tuning analysis...")
+
 tuningResult <- TuneClassifiers(
   studyCase = studyCase,
   dataType = dataType,
   tuneSVM = TRUE,
   tuneRandomForest = TRUE,
   tuneKNN = TRUE,
-  plotResults = TRUE)
+  plotResults = TRUE
+)
 
+# Export visualization plots --------------------------------------------------
 
-#### Export plots ####
-
-## Define directory and file prefix for the plots
+# Define output directory and file prefix for plots
 resultDir <- file.path(studyCase$parameters$dir$results, "tuning")
 dir.create(resultDir, showWarnings = FALSE, recursive = TRUE)
-filePrefix <- file.path(resultDir,
-                        paste0(
-                          studyCase$ID,
-                          "_", studyCase$parameters$feature,
-                          "_", dataType,
-                          "_tuning"))
 
-plotRFtuning(tuningResult, ylim = c(0,1), pdfFile = paste0(filePrefix, "_RF.pdf"))
-plotKNNtuning(tuningResult, ylim = c(0,1), pdfFile = paste0(filePrefix, "_KNN.pdf"))
+filePrefix <- file.path(
+  resultDir,
+  paste0(
+    studyCase$ID,
+    "_", studyCase$parameters$feature,
+    "_", dataType,
+    "_tuning"
+  )
+)
 
+# Generate tuning plots with consistent formatting
+plotRFtuning(tuningResult,
+             ylim = c(0, 1),
+             pdfFile = paste0(filePrefix, "_RF.pdf"))
 
-#### Save the results in a separate object, that can be reloaded later ####
+plotKNNtuning(tuningResult,
+              ylim = c(0, 1),
+              pdfFile = paste0(filePrefix, "_KNN.pdf"))
+
+# Save results -----------------------------------------------------------------
+
 message.with.time(
   "Saving results after parameter tuning: ",
-  save.result.file)
+  save.result.file
+)
+
 save(tuningResult, file = save.result.file)
 
-## Ending message
+# Completion message
 message.with.time("Finished script misc/05_tune_parameters.R")
-
